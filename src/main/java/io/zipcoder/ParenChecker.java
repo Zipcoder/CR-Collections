29b17274e15d88f311657bf02c97a96800b2425f
Anthony Jordan <Kain235@gmail.com>
2018-03-12 19:40:48 -0400

    Finished part 1 and 2

--- src/main/java/io/zipcoder/ParenChecker.java
+++ src/main/java/io/zipcoder/ParenChecker.java
@@ -1,4 +1,105 @@
 package io.zipcoder;
 
+import com.sun.xml.internal.fastinfoset.util.CharArray;
+
+import java.util.Stack;
+
 public class ParenChecker {
+    private Stack<Character> stackOfChars = new Stack();
+
+    public boolean parenChecker(String inputString) {
+        char[] arrayAsChar = inputString.toCharArray();
+        for (int i = 0; i < arrayAsChar.length; i++) {
+            characterChecker(arrayAsChar[i]);
+        }
+
+        if (stackOfChars.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void characterChecker(char inputChar) {
+        switch (inputChar) {
+            case '(':
+                stackOfChars.push(inputChar);
+                break;
+            case '{':
+                stackOfChars.push(inputChar);
+                break;
+            case '[':
+                stackOfChars.push(inputChar);
+                break;
+            case '<':
+                stackOfChars.push(inputChar);
+                break;
+            case ')':
+                if (!stackOfChars.empty()) {
+                    if (stackOfChars.peek().equals('(')) {
+                        stackOfChars.pop();
+                    } else {
+                        stackOfChars.push(inputChar);
+                    }
+                } else {
+                    stackOfChars.push(inputChar);
+                }
+                break;
+            case '}':
+                if (!stackOfChars.empty()) {
+                    if (stackOfChars.peek().equals('{')) {
+                        stackOfChars.pop();
+                    } else {
+                        stackOfChars.push(inputChar);
+                    }
+                } else {
+                    stackOfChars.push(inputChar);
+                }
+                break;
+            case ']':
+                if (!stackOfChars.empty()) {
+                    if (stackOfChars.peek().equals('[')) {
+                        stackOfChars.pop();
+                    } else {
+                        stackOfChars.push(inputChar);
+                    }
+                } else {
+                    stackOfChars.push(inputChar);
+                }
+                break;
+            case '>':
+                if (!stackOfChars.empty()) {
+                    if (stackOfChars.peek().equals('<')) {
+                        stackOfChars.pop();
+                    } else {
+                        stackOfChars.push(inputChar);
+                    }
+                } else {
+                    stackOfChars.push(inputChar);
+                }
+                break;
+            case '\"':
+                if (!stackOfChars.empty()) {
+                    if (stackOfChars.peek().equals('\"')) {
+                        stackOfChars.pop();
+                    } else {
+                        stackOfChars.push(inputChar);
+                    }
+                } else {
+                    stackOfChars.push(inputChar);
+                }
+                break;
+            case '\'':
+                if (!stackOfChars.empty()) {
+                    if (stackOfChars.peek().equals('\'')) {
+                        stackOfChars.pop();
+                    } else {
+                        stackOfChars.push(inputChar);
+                    }
+                } else {
+                    stackOfChars.push(inputChar);
+                }
+                break;
+        }
+    }
 }
--- src/test/java/io/zipcoder/ParenCheckerTest.java
+++ src/test/java/io/zipcoder/ParenCheckerTest.java
@@ -3,6 +3,37 @@ package io.zipcoder;
 import org.junit.Assert;
 import org.junit.Test;
 
+import javax.xml.ws.RequestWrapper;
+
 public class ParenCheckerTest {
 
+    @Test
+    public void parenCheckerTest1(){
+        ParenChecker testChecker = new ParenChecker();
+        boolean actual = testChecker.parenChecker("()()<>\"\"[]{}\'\'");
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void parenCheckerTest2(){
+        ParenChecker testChecker = new ParenChecker();
+        boolean actual = testChecker.parenChecker("ufeuifbbifeiu><<()");
+        Assert.assertFalse(actual);
+    }
+
+    @Test
+    public void parenCheckerTest3(){
+        ParenChecker testChecker = new ParenChecker();
+        boolean actual = testChecker.parenChecker("\"(({}))\"");
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void parenCheckerTest4(){
+        ParenChecker testChecker = new ParenChecker();
+        boolean actual = testChecker.parenChecker("fsf(kfbf{}jnje)<>\"dsdjhsjdhj\"");
+        Assert.assertTrue(actual);
+    }
+
 }
+
