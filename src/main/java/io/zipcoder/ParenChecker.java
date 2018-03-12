a527bcca91748675f32dfa84cce7f75c2c0d3bfd
Mitch Taylor <taylor.mitch.5@gmail.com>
2018-03-12 10:51:35 -0400

    ParenChecker part 1

--- src/main/java/io/zipcoder/ParenChecker.java
+++ src/main/java/io/zipcoder/ParenChecker.java
@@ -1,4 +1,33 @@
 package io.zipcoder;
 
+import java.util.Stack;
+
 public class ParenChecker {
+
+    private Stack charStack;
+
+    public ParenChecker(Stack charStack) {
+        this.charStack = charStack;
+    }
+
+    public boolean logic() {
+        int parenCount = 0;
+        for (int i = 0; i < charStack.size(); i++) {
+            if (parenCount < 0) {
+                return false;
+            }
+            if (charStack.get(i).equals('(')) {
+                parenCount++;
+            }
+            if (charStack.get(i).equals(')')) {
+                parenCount--;
+            }
+        }
+        if (parenCount != 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
 }
--- src/test/java/io/zipcoder/ParenCheckerTest.java
+++ src/test/java/io/zipcoder/ParenCheckerTest.java
@@ -3,6 +3,60 @@ package io.zipcoder;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class ParenCheckerTest {
+
+    @Test
+    public void parenthesisChecker1() {
+        Stack stack = new Stack();
+        ArrayList charArrayList = new ArrayList();
+        charArrayList.add('1');
+        charArrayList.add('d');
+        charArrayList.add('(');
+        charArrayList.add('f');
+        charArrayList.add('(');
+        charArrayList.add(')');
+        charArrayList.add(')');
+        stack.addAll(charArrayList);
+        ParenChecker parenChecker = new ParenChecker(stack);
+        Assert.assertTrue(parenChecker.logic());
+    }
+
+    @Test
+    public void parenthesisChecker2() {
+        Stack stack = new Stack();
+        ArrayList charArrayList = new ArrayList();
+        charArrayList.add('1');
+        charArrayList.add('d');
+        charArrayList.add('(');
+        charArrayList.add('f');
+        charArrayList.add('(');
+        charArrayList.add(')');
+        charArrayList.add(')');
+        charArrayList.add(')');
+        stack.addAll(charArrayList);
+        ParenChecker parenChecker = new ParenChecker(stack);
+        Assert.assertTrue(!(parenChecker.logic()));
+    }
+
+    @Test
+    public void parenthesisChecker3() {
+        Stack stack = new Stack();
+        ArrayList charArrayList = new ArrayList();
+        charArrayList.add(')');
+        charArrayList.add('1');
+        charArrayList.add('d');
+        charArrayList.add('(');
+        charArrayList.add('f');
+        charArrayList.add('(');
+        charArrayList.add(')');
+        charArrayList.add(')');
+        charArrayList.add(')');
+        stack.addAll(charArrayList);
+        ParenChecker parenChecker = new ParenChecker(stack);
+        Assert.assertTrue(!(parenChecker.logic()));
+    }
 
 }
