00159a5cf78d8111791fd99f770297266324841f
vvmk <vincentmasiello@gmail.com>
2018-03-12 17:05:40 -0400

    Generate word->frequency map
    tested w/ small,med,large input

--- src/main/java/io/zipcoder/WC.java
+++ src/main/java/io/zipcoder/WC.java
@@ -2,13 +2,18 @@ package io.zipcoder;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.nio.file.FileSystems;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.Scanner;
 
 public class WC {
+    private final String delimRegEx = "[^\\w]+";
     private Iterator<String> si;
+    private LinkedHashMap<String, Integer> hash;
 
     public WC(String fileName) {
+        hash = new LinkedHashMap<>();
         try {
             this.si = new Scanner(new FileReader(fileName));
         } catch (FileNotFoundException e) {
@@ -19,5 +24,31 @@ public class WC {
 
     public WC(Iterator<String> si) {
         this.si = si;
+        hash = new LinkedHashMap<>();
+    }
+
+
+    public void generate() {
+        while (si.hasNext()) {
+            String[] wordsThisLine = si.next().split(delimRegEx);
+            for (String raw : wordsThisLine) {
+                String key = raw.toLowerCase();
+
+                if (key.equals(""))
+                    continue;
+
+                Integer val = hash.get(key);
+                val = (val != null) ? (val + 1) : 1;
+                hash.put(key, val);
+            }
+        }
+    }
+    
+    public String toString() {
+        return hash.toString();
+    }
+
+    public void print() {
+        System.out.println(toString());
     }
 }
--- src/test/java/io/zipcoder/WCTest.java
+++ src/test/java/io/zipcoder/WCTest.java
@@ -1,11 +1,30 @@
 package io.zipcoder;
 
-import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.nio.file.FileSystems;
 
 public class WCTest {
 
+    @Test
+    public void testSmallInput() {
+        String name = "src/resources/small.txt";
+        WC wc = new WC(name);
+        wc.generate();
+        wc.print();
+    }
+
+    @Test
+    public void testMediumInput() {
+        WC wc = new WC("src/resources/frost.txt");
+        wc.generate();
+        wc.print();
+    }
+
+    @Test
+    public void testLargeInput() {
+        WC wc = new WC("src/resources/large.txt");
+        wc.generate();
+        wc.print();
+    }
 }
