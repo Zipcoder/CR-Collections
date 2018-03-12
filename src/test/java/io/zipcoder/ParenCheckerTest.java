93610ddc22bfda4638f083d8dc4b2784ccb59252
Garrett Arant <garrettarant@MacBook-Pro-4.local>
2018-03-12 13:00:56 -0400

    Completed Part 1 and 2

--- src/test/java/io/zipcoder/ParenCheckerTest.java
+++ src/test/java/io/zipcoder/ParenCheckerTest.java
@@ -3,6 +3,152 @@ package io.zipcoder;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Stack;
+
+import static io.zipcoder.ParenChecker.CheckParentesis;
+
 public class ParenCheckerTest {
+
+    @Test
+    public void parenCheckerTest1() {
+        // Given
+        Stack<Character> stack = new Stack<Character>();
+        String parenString = "()";
+
+        // When
+        Boolean actual = CheckParentesis(parenString);
+
+        // Then
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void parenCheckerTest2() {
+        // Given
+        Stack<Character> stack = new Stack<Character>();
+        String parenString = "()(";
+
+        // When
+        Boolean actual = CheckParentesis(parenString);
+
+        // Then
+        Assert.assertFalse(actual);
+    }
+
+    @Test
+    public void parenCheckerTest3() {
+        // Given
+        Stack<Character> stack = new Stack<Character>();
+        String parenString = ")()(";
+
+        //When
+        Assert.assertEquals(true, stack.isEmpty());
+        Boolean actual = CheckParentesis(parenString);
+
+        // Then
+        Assert.assertFalse(actual);
+    }
+
+    @Test
+    public void parenCheckerTest4() {
+        // Given
+        Stack<Character> stack = new Stack<Character>();
+        String parenString = "a(d)a";
+
+        //When
+        Assert.assertEquals(true, stack.isEmpty());
+        Boolean actual = CheckParentesis(parenString);
+
+        // Then
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void parenCheckerTest5() {
+        // Given
+        Stack<Character> stack = new Stack<Character>();
+        String parenString = "a(da";
+
+        //When
+        Assert.assertEquals(true, stack.isEmpty());
+        Boolean actual = CheckParentesis(parenString);
+
+        // Then
+        Assert.assertFalse(actual);
+    }
+
+    @Test
+    public void openingAndClosing1() {
+        // Given
+        Stack<Character> stack = new Stack<Character>();
+        String parenString = "({()})";
+
+        //When
+        Assert.assertEquals(true, stack.isEmpty());
+        Boolean actual = CheckParentesis(parenString);
+
+        // Then
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void openingAndClosing2() {
+        // Given
+        Stack<Character> stack = new Stack<Character>();
+        String parenString = "({)})";
+
+        //When
+        Assert.assertEquals(true, stack.isEmpty());
+        Boolean actual = CheckParentesis(parenString);
+
+        // Then
+        Assert.assertFalse(actual);
+    }
+
+    @Test
+    public void openingAndClosing3() {
+        // Given
+        Stack<Character> stack = new Stack<Character>();
+        String parenString = "\"\'({})\'\"";
+
+        //When
+        Assert.assertEquals(true, stack.isEmpty());
+        Boolean actual = CheckParentesis(parenString);
+
+        // Then
+        Assert.assertTrue(actual);
+    }
+
+
+    @Test
+    public void openingAndClosing4() {
+        // Given
+        Stack<Character> stack = new Stack<Character>();
+        String parenString = "c\"c\'c(d{a}d)d\'c\"c";
+
+        //When
+        Assert.assertEquals(true, stack.isEmpty());
+        Boolean actual = CheckParentesis(parenString);
+
+        // Then
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void openingAndClosing5() {
+        // Given
+        Stack<Character> stack = new Stack<Character>();
+        String parenString = "c\"c\'c}d)d\'c\"c";
+
+        //When
+        Assert.assertEquals(true, stack.isEmpty());
+        Boolean actual = CheckParentesis(parenString);
+
+        // Then
+        Assert.assertFalse(actual);
+    }
+
+
+
 
 }
