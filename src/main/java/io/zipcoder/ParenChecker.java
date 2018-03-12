54abbb2d9fde032a4272dc569fe73ee4be175852
Eric Barnaba <ericbarnaba@zipcoders-MacBook-Pro-25.local>
2018-03-12 13:27:19 -0400

    saving

--- src/main/java/io/zipcoder/ParenChecker.java
+++ src/main/java/io/zipcoder/ParenChecker.java
@@ -1,4 +1,24 @@
 package io.zipcoder;
 
+import java.util.Stack;
+
 public class ParenChecker {
+
+    public static boolean parenthesesAreAllClosed(String stringToTest){
+        Stack<Character> stackOfParentheses = new Stack<>();
+        for(int i = 0; i < stringToTest.length(); i++){
+            if(stringToTest.charAt(i) == '('){
+                stackOfParentheses.push(stringToTest.charAt(i));
+            }
+            else if(stringToTest.charAt(i) == ')' && stackOfParentheses.contains('(')){
+                stackOfParentheses.pop();
+            }
+            else if(stringToTest.charAt(i) == ')'){
+                stackOfParentheses.push(stringToTest.charAt(i));
+            }
+        }
+
+        return stackOfParentheses.isEmpty();
+    }
+
 }
--- src/test/java/io/zipcoder/ParenCheckerTest.java
+++ src/test/java/io/zipcoder/ParenCheckerTest.java
@@ -5,4 +5,49 @@ import org.junit.Test;
 
 public class ParenCheckerTest {
 
+    @Test
+    public void openAndCloseTest(){
+        //Given
+        String test = "aBcD(eFg)HiJ(kLmN)";
+
+        //When
+
+        //Then
+        Assert.assertTrue(ParenChecker.parenthesesAreAllClosed(test));
+
+    }
+
+    @Test
+    public void openNoCloseTest(){
+        //Given
+        String test = "aBcD(eFgHiJ(kLmN)";
+
+        //When
+
+        //Then
+        Assert.assertFalse(ParenChecker.parenthesesAreAllClosed(test));
+    }
+
+    @Test
+    public void closeNoOpenTest(){
+        //Given
+        String test = "aBcD()eFgHiJkLmN)";
+
+        //When
+
+        //Then
+        Assert.assertFalse(ParenChecker.parenthesesAreAllClosed(test));
+    }
+
+    @Test
+    public void closeNoOpenTest2(){
+        //Given
+        String test = ")aBcD()eFgHiJkLmN()";
+
+        //When
+
+        //Then
+        Assert.assertFalse(ParenChecker.parenthesesAreAllClosed(test));
+    }
+
 }
