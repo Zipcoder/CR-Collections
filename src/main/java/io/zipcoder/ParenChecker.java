341f02a7c3aecacebd79f3a651f5d0865ae2e941
Patrick Glavin <patjglavin@gmail.com>
2018-03-12 21:14:35 -0400

    add parenchecker

--- src/main/java/io/zipcoder/ParenChecker.java
+++ src/main/java/io/zipcoder/ParenChecker.java
@@ -1,4 +1,30 @@
 package io.zipcoder;
 
+import java.util.Stack;
+
 public class ParenChecker {
+    public String string;
+    public Stack<String> openParen;
+    public Stack<String> closeParen;
+
+    public ParenChecker(String entry){
+        this.string = entry;
+        this.openParen = new Stack<String>();
+        this.closeParen = new Stack<String>();
+    }
+
+    public boolean checkParen(){
+        for (String letter:string.split("")) {
+            if (letter.equals("(")){
+                openParen.push(letter);
+            }
+            if (letter.equals(")")){
+                closeParen.push(letter);
+                if (openParen.size()<closeParen.size()) return false;
+            }
+        }
+        if (openParen.size() != closeParen.size()) return false;
+        return true;
+    }
+
 }
--- src/test/java/io/zipcoder/ParenCheckerTest.java
+++ src/test/java/io/zipcoder/ParenCheckerTest.java
@@ -1,8 +1,38 @@
 package io.zipcoder;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ParenCheckerTest {
+    ParenChecker parenChecker;
+
+    @Before
+    public void setup(){
+        parenChecker = new ParenChecker("er(gerg)werwerew");
+    }
+
+    @Test
+    public void checkParenTest(){
+        Assert.assertTrue(parenChecker.checkParen());
+    }
+
+    @Test
+    public void checkParenTest2(){
+        parenChecker = new ParenChecker("wuiebfuiewb((alhwbef)");
+        Assert.assertFalse(parenChecker.checkParen());
+    }
+
+    @Test
+    public void checkParenTest3(){
+        parenChecker = new ParenChecker("wuiebfuiew)b((alhwbef)");
+        Assert.assertFalse(parenChecker.checkParen());
+    }
+
+    @Test
+    public void checkParenTest4(){
+        parenChecker = new ParenChecker("wuiebfui(ew)b((alhwbef)");
+        Assert.assertFalse(parenChecker.checkParen());
+    }
 
 }
