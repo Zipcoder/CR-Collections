c5868068415937d0faca62720a71a60e4a6bd59e
Kibret Tecle <kibrettecle@zipcoders-MacBook-Pro-5.local>
2018-03-12 23:24:46 -0400

    saving

--- src/main/java/io/zipcoder/ParenChecker.java
+++ src/main/java/io/zipcoder/ParenChecker.java
@@ -1,4 +1,18 @@
 package io.zipcoder;
 
+import java.util.Stack;
+
 public class ParenChecker {
+
+    public static boolean parenChecker(String aString){
+        Stack myStack = new Stack();
+        for(int i=0;i<aString.length();i++){
+            if(aString.charAt(i)=='('){
+               myStack.add(aString.charAt(i));
+            }else if(!(myStack.isEmpty()) && aString.charAt(i)==')'){
+                myStack.pop();
+            }
+        }
+        return myStack.isEmpty();
+    }
 }
--- src/test/java/io/zipcoder/ParenCheckerTest.java
+++ src/test/java/io/zipcoder/ParenCheckerTest.java
@@ -3,6 +3,36 @@ package io.zipcoder;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Stack;
+
 public class ParenCheckerTest {
+
+    @Test
+    public void testParenChecker1(){
+        boolean actual = ParenChecker.parenChecker("(");
+        Assert.assertFalse(actual);
+    }
+
+    @Test
+    public void testPareChecker2(){
+       boolean actual =  ParenChecker.parenChecker("ahfas(;fjjljl)hdhjfj777(kjjg)dfkj");
+       Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void testPareChecker3(){
+        boolean actual = ParenChecker.parenChecker("<hjadhfd>)haddhjk(jkl(lkdja)");
+        Assert.assertFalse(actual);
+    }
+    @Test
+    public void testParenChecker4(){
+        boolean actual = ParenChecker.parenChecker("))))))))))ajhdfdha;(((9");
+        Assert.assertFalse(actual);
+    }
+    @Test
+    public void testParenChecker5(){
+        boolean actual = ParenChecker.parenChecker("eyioewncnv(((hgajdhak)adnkjf)euei)");
+        Assert.assertTrue(actual);
+    }
 
 }
