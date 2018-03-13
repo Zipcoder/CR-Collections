e8f23d6483e25b4cd0af5f243c04e3cc4c6b07cf
Eric Barnaba <ericbarnaba@zipcoders-MacBook-Pro-25.local>
2018-03-13 16:45:12 -0400

    refactored

--- src/main/java/io/zipcoder/WC.java
+++ src/main/java/io/zipcoder/WC.java
@@ -17,86 +17,65 @@ public class WC {
         }
     }
 
-
     public WC(Iterator<String> si) {
         this.si = si;
     }
 
+    private static Comparator<Map.Entry<String, Integer>> byCountThenByName = (o1, o2) -> {
+        if (o2.getValue().compareTo(o1.getValue()) == 0) return o1.getKey().compareTo(o2.getKey());
+        else return o2.getValue().compareTo(o1.getValue());
+    };
+
 
     public ArrayList<Map.Entry<String, Integer>> countWordsWithoutLinking() {
         HashMap<String, Integer> outputMap = new HashMap<>();
-        int totalWords = 0;
 
-        while (si.hasNext()) {
-            totalWords++;
-            String word = si.next().toLowerCase().replaceAll("[^a-z]", "");
-            if (outputMap.containsKey(word)) {
-                int newNumber = outputMap.get(word) + 1;
-                outputMap.put(word, newNumber);
-            } else {
-                outputMap.put(word, 1);
-            }
-        }
-        outputMap.put("TOTAL WORDS", totalWords);
+        outputMap = (HashMap<String, Integer>) buildMap(outputMap);
 
         ArrayList<Map.Entry<String, Integer>> outputList = new ArrayList<>(outputMap.entrySet());
-        outputList.sort((o1, o2) -> {
-            if (o2.getValue().compareTo(o1.getValue()) == 0) return o1.getKey().compareTo(o2.getKey());
-            else return o2.getValue().compareTo(o1.getValue());
-        });
+        outputList.sort(byCountThenByName);
 
         return outputList;
     }
 
     public LinkedList<Map.Entry<String, Integer>> countWordsWithLinking() {
         LinkedHashMap<String, Integer> outputMap = new LinkedHashMap<>();
-        int totalWords = 0;
 
-        while (si.hasNext()) {
-            totalWords++;
-            String word = si.next().toLowerCase().replaceAll("[^a-z]", "");
-            if (outputMap.containsKey(word)) {
-                int newNumber = outputMap.get(word) + 1;
-                outputMap.put(word, newNumber);
-            } else {
-                outputMap.put(word, 1);
-            }
-        }
-        outputMap.put("TOTAL WORDS", totalWords);
+        outputMap = (LinkedHashMap<String, Integer>) buildMap(outputMap);
 
         LinkedList<Map.Entry<String, Integer>> outputList = new LinkedList<>(outputMap.entrySet());
-        outputList.sort((o1, o2) -> {
-            if (o2.getValue().compareTo(o1.getValue()) == 0) return o1.getKey().compareTo(o2.getKey());
-            else return o2.getValue().compareTo(o1.getValue());
-        });
+        outputList.sort(byCountThenByName);
 
         return outputList;
     }
 
     public TreeSet<Map.Entry<String, Integer>> countWordsWithTreeSet() {
         LinkedHashMap<String, Integer> outputMap = new LinkedHashMap<>();
-        int totalWords = 0;
 
-        while (si.hasNext()) {
-            totalWords++;
-            String word = si.next().toLowerCase().replaceAll("[^a-z]", "");
-            if (outputMap.containsKey(word)) {
-                int newNumber = outputMap.get(word) + 1;
-                outputMap.put(word, newNumber);
-            } else {
-                outputMap.put(word, 1);
-            }
-        }
-        outputMap.put("TOTAL WORDS", totalWords);
+        outputMap = (LinkedHashMap<String, Integer>) buildMap(outputMap);
 
-        TreeSet<Map.Entry<String, Integer>> outputSet = new TreeSet<>((o1, o2) -> {
-            if (o2.getValue().compareTo(o1.getValue()) == 0) return o1.getKey().compareTo(o2.getKey());
-            else return o2.getValue().compareTo(o1.getValue());
-        });
+        TreeSet<Map.Entry<String, Integer>> outputSet = new TreeSet<>(byCountThenByName);
         outputSet.addAll(outputMap.entrySet());
 
         return outputSet;
     }
+
+    private Map<String,Integer> buildMap (Map<String,Integer> inputMap){
+        int totalWords = 0;
+        while (si.hasNext()) {
+            totalWords++;
+            String word = si.next().toLowerCase().replaceAll("[^a-z]", "");
+            if (inputMap.containsKey(word)) {
+                int newNumber = inputMap.get(word) + 1;
+                inputMap.put(word, newNumber);
+            } else {
+                inputMap.put(word, 1);
+            }
+        }
+        inputMap.put("TOTAL WORDS", totalWords);
+        return inputMap;
+    }
+
 
 }
 
