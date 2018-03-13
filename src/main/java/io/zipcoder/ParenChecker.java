0586e5d117407099c8a94ec5f8d0c25d49c4225a
Bo Lee <bolee@zipcoders-MacBook-Pro-7.local>
2018-03-13 16:40:20 -0400

    Complete

--- src/main/java/io/zipcoder/ParenChecker.java
+++ src/main/java/io/zipcoder/ParenChecker.java
@@ -1,4 +1,71 @@
 package io.zipcoder;
 
+import java.util.Stack;
+
 public class ParenChecker {
+    public Stack<Character> stack = new Stack<Character>();
+
+
+    public ParenChecker(){
+
+    }
+    public ParenChecker(Stack<Character> stack) {
+        this.stack = stack;
+    }
+
+    public boolean isPairedParenthesis(String str){
+        for (int i = 0; i <str.length() ; i++) {
+            char current = str.charAt(i);
+            if (current == '(') {
+                stack.push(current);
+            } else if (current == ')') {
+                if (stack.isEmpty()){
+                    return false;
+                }
+                    else if (stack.peek() == '(') {
+                    stack.pop();
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+
+
+
+    public boolean isBalancedBrackets(String str){
+        for (int i = 0; i <str.length() ; i++) {
+            char current = str.charAt(i);
+            if (current == '(' ||current == '{' ||current == '[' ||current == '<' ||
+                    current == '\"' ||current == '\'' ) {
+                stack.push(current);
+            } else if (current == ')'||current == '}' ||current == ']' ||current == '>' ||
+                    current == '"' ||current == '\'' ) {
+                if (stack.isEmpty()){
+                    return false;
+                }
+                else if (stack.peek() == '(') {
+                    stack.pop();
+                }
+                else if (stack.peek() == '{') {
+                    stack.pop();
+                }
+                else if (stack.peek() == '[') {
+                    stack.pop();
+                }
+                else if (stack.peek() == '<') {
+                    stack.pop();
+                }
+                else if (stack.peek() == '\"') {
+                    stack.pop();
+                }
+                else if (stack.peek() == '\'') {
+                    stack.pop();
+                }
+            }
+        }
+        return stack.isEmpty();
+
+
+
+    }
 }
--- src/test/java/io/zipcoder/ParenCheckerTest.java
+++ src/test/java/io/zipcoder/ParenCheckerTest.java
@@ -5,4 +5,87 @@ import org.junit.Test;
 
 public class ParenCheckerTest {
 
+    @Test
+    public void isPairedParenthesis1(){
+        ParenChecker parenChecker = new ParenChecker();
+        String str = "()()()()()";
+
+        boolean expected = true;
+        boolean actual = parenChecker.isPairedParenthesis(str);
+
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void isPairedParenthesis2(){
+        ParenChecker parenChecker = new ParenChecker();
+        String str = ")()()()()()";
+
+        boolean expected = false;
+        boolean actual = parenChecker.isPairedParenthesis(str);
+
+        Assert.assertEquals(expected, actual);
+
+    }
+    @Test
+    public void isPairedParenthesis3(){
+        ParenChecker parenChecker = new ParenChecker();
+        String str = "))()()()()";
+
+        boolean expected = false;
+        boolean actual = parenChecker.isPairedParenthesis(str);
+
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void isBalancedBrackets1(){
+        ParenChecker parenChecker = new ParenChecker();
+        String str = "([])({})(<>)()()";
+
+        boolean expected = true;
+        boolean actual = parenChecker.isPairedParenthesis(str);
+
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void isBalancedBrackets2(){
+        ParenChecker parenChecker = new ParenChecker();
+        String str = "()}{()}{(){}()[]()";
+
+        boolean expected = false;
+        boolean actual = parenChecker.isPairedParenthesis(str);
+
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void isBalancedBrackets3(){
+        ParenChecker parenChecker = new ParenChecker();
+        String str = "[](){}()()()()";
+
+        boolean expected = true;
+        boolean actual = parenChecker.isPairedParenthesis(str);
+
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void isBalancedBrackets4(){
+        ParenChecker parenChecker = new ParenChecker();
+        String str = "({})(<>)()()()";
+
+        boolean expected = true;
+        boolean actual = parenChecker.isPairedParenthesis(str);
+
+        Assert.assertEquals(expected, actual);
+
+    }
+
 }
