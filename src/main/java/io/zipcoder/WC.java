8e3eb5a98b5d44641469efc67bd35d1eda8c3e19
Carolynn Vansant <cvansant@MacBook-Pro-5.local>
2018-03-13 17:42:02 -0400

    finished WC, putting in pull request

--- src/main/java/io/zipcoder/WC.java
+++ src/main/java/io/zipcoder/WC.java
@@ -2,17 +2,16 @@ package io.zipcoder;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.math.BigInteger;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.math.BigInteger.valueOf;
 
 public class WC {
 
     private Iterator<String> si;
-    private LinkedHashMap<BigInteger, String> wordCount;
-    private ArrayList stringList;
-
+    private LinkedHashMap<String, Integer> wordCount;
 
     public WC(String fileName) {
         try {
@@ -21,22 +20,36 @@ public class WC {
             System.out.println(fileName + " Does Not Exist");
             System.exit(-1);
         }
+        this.wordCount = new LinkedHashMap<>();
     }
 
     public WC(Iterator<String> si) {
         this.si = si;
     }
 
-    public LinkedHashMap<BigInteger, String> createMap(){
-        return null;
+    public void createMap() {
+        while (si.hasNext()) {
+            si.hasNext();
+            String input = si.next();
+            String element = input.replaceAll("\\W+", "").toLowerCase();
+            if(!wordCount.containsKey(element)) {
+                wordCount.put(element, 1);
+            } else if(wordCount.containsKey(element)) {
+                wordCount.replace(element, wordCount.get(element)+1);
+            }
+        }
     }
 
-    public String printMap(){
-        return null;
-    }
 
-    public String printSubString(Integer numberOfMapEntries){
-        return null;
+    public StringBuilder printMap(){
+        StringBuilder count = new StringBuilder();
+        List<Map.Entry<String,Integer>> list = new ArrayList<>(wordCount.entrySet());
+        list.sort(Comparator.comparing(Map.Entry::getValue));
+        Collections.reverse(list);
+        for(Map.Entry<String,Integer> entry: list){
+            count.append(entry.getKey()).append(": ").append(entry.getValue()).append("\n");
+        }
+        return count;
     }
 
 }
--- src/test/java/io/zipcoder/WCTest.java
+++ src/test/java/io/zipcoder/WCTest.java
@@ -9,25 +9,14 @@ import java.util.Arrays;
 public class WCTest {
 
     @Test
-    public void createMapTest(){
+    public void createMapTest() {
         //Given
         WC wordCount = new WC(WC.class.getResource("/244-0.txt").getFile());
         //When
         wordCount.createMap();
         //Then
-        wordCount.printSubString(15);
-
-
-    }
-
-    @Test
-    public void printMapTest(){
-        //Given
-
-        //When
-
-        //Then
-
+        StringBuilder counts = wordCount.printMap();
+        System.out.println(counts);
     }
 
 }
