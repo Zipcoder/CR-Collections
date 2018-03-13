55159a1f346591f31719ad005f118848954a287a
Patrick Glavin <patjglavin@gmail.com>
2018-03-13 14:24:40 -0400

    refactor into smaller methods

--- src/main/java/io/zipcoder/ParenChecker.java
+++ src/main/java/io/zipcoder/ParenChecker.java
@@ -1,6 +1,5 @@
 package io.zipcoder;
 
-import java.text.StringCharacterIterator;
 import java.util.*;
 
 public class ParenChecker {
@@ -45,43 +44,58 @@ public class ParenChecker {
     public boolean checkCharacters(){
         String[] tempArray = string.split("");
         ArrayList<Enclosers> enclosersList = new ArrayList<Enclosers>(Arrays.asList(Enclosers.values()));
-        ArrayList<String> openerList = new ArrayList<String>();
         ArrayList<String> closerList = new ArrayList<String>();
         for (Enclosers encloser:enclosersList) {
-            openerList.add(encloser.getOpen());
             closerList.add(encloser.getClose());
         }
         for (String ch:tempArray){
-            for (Enclosers encloser:enclosersList) {
-                if (ch.equals(encloser.getOpen())) enclosers.push(encloser);
-            }
-            if (closerList.contains(ch)){
-                try {
-                    if (ch.equals(enclosers.peek().getClose())) enclosers.pop();
-                    else return false;
-                } catch (EmptyStackException exception) {
-                    return false;
-                }
-            }
-            if (ch.equals("'")){
-                if (quotes.size() == 1) {
-                    quotes.pop();
-                } else if (quotes.size() == 0){
-                    quotes.push(ch);
-                }
-            }
-            if (ch.equals("\'")){
-                if (doublequotes.size() == 1) {
-                    doublequotes.pop();
-                } else if (doublequotes.size() == 0){
-                    doublequotes.push(ch);
-                }
-            }
+            checkOpener(enclosersList, ch);
+            if (checkCloser(closerList, ch)) return false;
+            checkSingleQuote(ch);
+            checkDoubleQuote(ch);
         }
         if (enclosers.size() > 0) return false;
         if (quotes.size() > 0) return false;
         if (doublequotes.size() > 0) return false;
         return true;
+    }
+
+    private void checkDoubleQuote(String ch) {
+        if (ch.equals("\'")){
+            if (doublequotes.size() == 1) {
+                doublequotes.pop();
+            } else if (doublequotes.size() == 0){
+                doublequotes.push(ch);
+            }
+        }
+    }
+
+    private void checkSingleQuote(String ch) {
+        if (ch.equals("'")){
+            if (quotes.size() == 1) {
+                quotes.pop();
+            } else if (quotes.size() == 0){
+                quotes.push(ch);
+            }
+        }
+    }
+
+    private boolean checkCloser(ArrayList<String> closerList, String ch) {
+        if (closerList.contains(ch)){
+            try {
+                if (ch.equals(enclosers.peek().getClose())) enclosers.pop();
+                else return true;
+            } catch (EmptyStackException exception) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void checkOpener(ArrayList<Enclosers> enclosersList, String ch) {
+        for (Enclosers encloser:enclosersList) {
+            if (ch.equals(encloser.getOpen())) enclosers.push(encloser);
+        }
     }
 
 }
