7551101fd85ad6420d9ce4834b721f67360bcdca
Eric Barnaba <ericbarnaba@zipcoders-MacBook-Pro-25.local>
2018-03-13 16:12:21 -0400

    tree set is the fastest

--- src/main/java/io/zipcoder/MainApplication.java
+++ src/main/java/io/zipcoder/MainApplication.java
@@ -3,6 +3,7 @@ package io.zipcoder;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Map;
+import java.util.TreeSet;
 
 public class MainApplication {
 
@@ -36,6 +37,21 @@ public class MainApplication {
         System.out.println("The Adventures of Huckleberry Finn: " + words6);
         long duration2 = (System.nanoTime()-startTime2)/1000000;
         System.out.println("Process took " + duration2 + "ms to compute");
+        System.out.println();
+
+        System.out.println("Running tests with TreeSet:");
+        long startTime3 = System.nanoTime();
+        WC goodAndEvil3 = new WC(WC.class.getResource("/BeyondGoodAndEvil.txt").getFile());
+        TreeSet<Map.Entry<String,Integer>> words7 = goodAndEvil3.countWordsWithTreeSet();
+        System.out.println("Beyond Good and Evil: " + words7);
+        WC earnest3 = new WC(WC.class.getResource("/TheImportanceOfBeingEarnest.txt").getFile());
+        TreeSet<Map.Entry<String,Integer>> words8 = earnest3.countWordsWithTreeSet();
+        System.out.println("The Importance of Being Earnest: " + words8);
+        WC huckFinn3 = new WC(WC.class.getResource("/TheAdventuresOfHuckleberryFinn").getFile());
+        TreeSet<Map.Entry<String,Integer>> words9 = huckFinn3.countWordsWithTreeSet();
+        System.out.println("The Adventures of Huckleberry Finn: " + words9);
+        long duration3 = (System.nanoTime()-startTime3)/1000000;
+        System.out.println("Process took " + duration3 + "ms to compute");
     }
 
 
--- src/main/java/io/zipcoder/WC.java
+++ src/main/java/io/zipcoder/WC.java
@@ -17,62 +17,87 @@ public class WC {
         }
     }
 
+
     public WC(Iterator<String> si) {
         this.si = si;
     }
 
 
-    public ArrayList <Map.Entry<String,Integer>> countWordsWithoutLinking(){
-        HashMap<String,Integer> outputMap = new HashMap<>();
-        int totalWords =0;
+    public ArrayList<Map.Entry<String, Integer>> countWordsWithoutLinking() {
+        HashMap<String, Integer> outputMap = new HashMap<>();
+        int totalWords = 0;
 
-        while(si.hasNext()){
+        while (si.hasNext()) {
             totalWords++;
-            String word = si.next().toLowerCase().replaceAll("[^a-z]","");
-            if(outputMap.containsKey(word)){
-                int newNumber = outputMap.get(word)+1;
+            String word = si.next().toLowerCase().replaceAll("[^a-z]", "");
+            if (outputMap.containsKey(word)) {
+                int newNumber = outputMap.get(word) + 1;
                 outputMap.put(word, newNumber);
-            }
-            else{
-                outputMap.put(word,1);
+            } else {
+                outputMap.put(word, 1);
             }
         }
         outputMap.put("TOTAL WORDS", totalWords);
 
-        ArrayList <Map.Entry<String,Integer>> outputList = new ArrayList<>(outputMap.entrySet());
-        outputList.sort((o1,o2) -> {
-            if(o2.getValue().compareTo(o1.getValue()) == 0) return o1.getKey().compareTo(o2.getKey());
+        ArrayList<Map.Entry<String, Integer>> outputList = new ArrayList<>(outputMap.entrySet());
+        outputList.sort((o1, o2) -> {
+            if (o2.getValue().compareTo(o1.getValue()) == 0) return o1.getKey().compareTo(o2.getKey());
             else return o2.getValue().compareTo(o1.getValue());
         });
 
         return outputList;
     }
 
-    public LinkedList <Map.Entry<String,Integer>> countWordsWithLinking(){
-        LinkedHashMap<String,Integer> outputMap = new LinkedHashMap<>();
-        int totalWords =0;
+    public LinkedList<Map.Entry<String, Integer>> countWordsWithLinking() {
+        LinkedHashMap<String, Integer> outputMap = new LinkedHashMap<>();
+        int totalWords = 0;
 
-        while(si.hasNext()){
+        while (si.hasNext()) {
             totalWords++;
-            String word = si.next().toLowerCase().replaceAll("[^a-z]","");
-            if(outputMap.containsKey(word)){
-                int newNumber = outputMap.get(word)+1;
+            String word = si.next().toLowerCase().replaceAll("[^a-z]", "");
+            if (outputMap.containsKey(word)) {
+                int newNumber = outputMap.get(word) + 1;
                 outputMap.put(word, newNumber);
-            }
-            else{
-                outputMap.put(word,1);
+            } else {
+                outputMap.put(word, 1);
             }
         }
         outputMap.put("TOTAL WORDS", totalWords);
 
-        LinkedList <Map.Entry<String,Integer>> outputList = new LinkedList<>(outputMap.entrySet());
-        outputList.sort((o1,o2) -> {
-            if(o2.getValue().compareTo(o1.getValue()) == 0) return o1.getKey().compareTo(o2.getKey());
+        LinkedList<Map.Entry<String, Integer>> outputList = new LinkedList<>(outputMap.entrySet());
+        outputList.sort((o1, o2) -> {
+            if (o2.getValue().compareTo(o1.getValue()) == 0) return o1.getKey().compareTo(o2.getKey());
             else return o2.getValue().compareTo(o1.getValue());
         });
 
         return outputList;
+    }
+
+    public TreeSet<Map.Entry<String, Integer>> countWordsWithTreeSet() {
+        LinkedHashMap<String, Integer> outputMap = new LinkedHashMap<>();
+        int totalWords = 0;
+
+        while (si.hasNext()) {
+            totalWords++;
+            String word = si.next().toLowerCase().replaceAll("[^a-z]", "");
+            if (outputMap.containsKey(word)) {
+                int newNumber = outputMap.get(word) + 1;
+                outputMap.put(word, newNumber);
+            } else {
+                outputMap.put(word, 1);
+            }
+        }
+        outputMap.put("TOTAL WORDS", totalWords);
+
+        TreeSet<Map.Entry<String, Integer>> outputSet = new TreeSet<>((o1, o2) -> {
+            if (o2.getValue().compareTo(o1.getValue()) == 0) return o1.getKey().compareTo(o2.getKey());
+            else return o2.getValue().compareTo(o1.getValue());
+        });
+        outputSet.addAll(outputMap.entrySet());
+
+        return outputSet;
     }
 
 }
+
 
