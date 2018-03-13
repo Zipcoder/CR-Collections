53954e2eba9e3d8a078e43e429c2af7ff988ee49
CWinarski <Winarski70@yahoo.com>
2018-03-13 00:21:26 -0400

    Finished Part 1

--- src/main/java/io/zipcoder/ParenChecker.java
+++ src/main/java/io/zipcoder/ParenChecker.java
@@ -1,4 +1,27 @@
 package io.zipcoder;
 
-public class ParenChecker {
+
+import java.util.Stack;
+
+public class ParenChecker{
+
+    Stack<Character> parensStack = new Stack<Character>();
+
+    public boolean verifyParensArePaired(String input){
+       for(int i = 0; i < input.length(); i++){
+           char inputCharacter = input.charAt(i);
+           if (inputCharacter == '('){
+               parensStack.push(inputCharacter);
+           }else if (inputCharacter == ')' &&  parensStack.isEmpty() || parensStack.peek() != '('){
+               return false;
+           } else if (inputCharacter == ')' && parensStack.peek() == '('){
+            parensStack.pop();
+        }
+       }
+        return parensStack.isEmpty();
+    }
+
+    public boolean verifyOpeningCharactersHaveAClosingOne(String input){
+        return false;
+    }
 }
--- src/test/java/io/zipcoder/ParenCheckerTest.java
+++ src/test/java/io/zipcoder/ParenCheckerTest.java
@@ -4,5 +4,78 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class ParenCheckerTest {
+    ParenChecker parenCheckerTest = new ParenChecker();
+    @Test
+    public void verifyParensArePairedTest(){
+        //Given
+        String test = "()";
+        boolean expected = true;
+        //When
+        boolean actual = parenCheckerTest.verifyParensArePaired(test);
+        Assert.assertEquals(expected,actual);
+    }
 
+    @Test
+    public void verifyParensArePairedTest2(){
+        //Given
+        String test = "((";
+        boolean expected = false;
+        //When
+        boolean actual = parenCheckerTest.verifyParensArePaired(test);
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void verifyParensArePairedTest3(){
+        //Given
+        String test = ")(";
+        boolean expected = false;
+        //When
+        boolean actual = parenCheckerTest.verifyParensArePaired(test);
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void verifyParensArePairedTest4(){
+        //Given
+        String test = "(";
+        boolean expected = false;
+        //When
+        boolean actual = parenCheckerTest.verifyParensArePaired(test);
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void verifyOpeningCharactersHaveAClosingOneTest(){
+
+    }
+    @Test
+    public void verifyOpeningCharactersHaveAClosingOneTest2(){
+        //Given
+        String test = "()";
+        boolean expected = true;
+        //When
+        boolean actual = parenCheckerTest.verifyParensArePaired(test);
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void verifyOpeningCharactersHaveAClosingOneTest3(){
+        //Given
+        String test = "((";
+        boolean expected = false;
+        //When
+        boolean actual = parenCheckerTest.verifyParensArePaired(test);
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void verifyOpeningCharactersHaveAClosingOneTest4(){
+        //Given
+        String test = ")(";
+        boolean expected = false;
+        //When
+        boolean actual = parenCheckerTest.verifyParensArePaired(test);
+        Assert.assertEquals(expected,actual);
+    }
 }
