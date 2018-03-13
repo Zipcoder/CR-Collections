786f2d3d06e10604318c8e28351de18c1b48c162
Patrick Glavin <patjglavin@gmail.com>
2018-03-13 14:47:34 -0400

    bad file path

--- src/test/java/io/zipcoder/WCTest.java
+++ src/test/java/io/zipcoder/WCTest.java
@@ -1,11 +1,34 @@
 package io.zipcoder;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+import java.lang.reflect.Field;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class WCTest {
 
+    File file = new File("../resources/someTextFile.txt");
+    WC wc;
+
+    @Before
+    public void setup(){
+        wc = new WC("/Users/patrickglavin/Dev/third_hex/EncapsulativeCharacters/src/main/resources/someTextFile.txt");
+        wc = new WC("/../../../resources/someTextFile.txt");
+    }
+
+    @Test
+    public void constructorTest(){
+
+    }
+
+    // /Users/patrickglavin/Dev/third_hex/EncapsulativeCharacters/src/main/java/io/zipcoder/WC.java
+
+
+    // /Users/patrickglavin/Dev/third_hex/EncapsulativeCharacters/src/main/java/io/zipcoder/WC.java
+    // /Users/patrickglavin/Dev/third_hex/EncapsulativeCharacters/src/main/resources/someTextFile.txt
 }
