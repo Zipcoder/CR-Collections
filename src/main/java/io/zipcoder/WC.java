cc8c33f31d246e2b5958dc0b69d313594f0ae3b9
Lawrence Wu <yl981@scarletmail.rutgers.edu>
2018-03-12 22:21:57 -0400

    Complete

--- src/main/java/io/zipcoder/WC.java
+++ src/main/java/io/zipcoder/WC.java
@@ -3,11 +3,12 @@ package io.zipcoder;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.Iterator;
-import java.util.Scanner;
+import java.util.*;
 
 public class WC {
     private Iterator<String> si;
+    private LinkedHashMap<String, Integer> wordCount;
+    private String separator = "[^\\w]+";
 
     public WC(String fileName) {
         try {
@@ -16,8 +17,50 @@ public class WC {
             System.out.println(fileName + " Does Not Exist");
             System.exit(-1);
         }
+        wordCount = new LinkedHashMap<>();
     }
     public WC(Iterator<String> si) {
         this.si = si;
+        wordCount = new LinkedHashMap<>();
+    }
+
+    public void textIterator(){
+        wordCount = new LinkedHashMap<>();
+        while (si.hasNext()){
+            //put all the words in the text into a string[].
+            String[] allTextSeparated = si.next().toLowerCase().split(separator);
+            for (String word : allTextSeparated){
+                if (wordCount.containsKey(word)){
+                    int value = wordCount.get(word) + 1;
+                    wordCount.put(word, value);
+                }
+                else {
+                    wordCount.put(word, 1);
+                }
+            }
+        }
+    }
+    public List <Map.Entry<String, Integer>> sorted(){
+        List<Map.Entry<String, Integer>> sortedMap = new ArrayList<>(wordCount.entrySet());
+        Collections.sort(sortedMap, new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                return (o2.getValue()).compareTo(o1.getValue());
+            }
+
+        });
+        for(Map.Entry<String, Integer> entry: sortedMap){
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+        return sortedMap;
+    }
+    public String toString(){
+        StringBuilder usedForTest = new StringBuilder();
+        sorted().forEach((Map.Entry<String, Integer> e) ->
+            usedForTest.append(e.getKey() + " : " + e.getValue() + "\n"));
+            return usedForTest.toString();
+    }
+    public void print(){
+        System.out.println(toString());
     }
 }
--- src/test/java/io/zipcoder/WCTest.java
+++ src/test/java/io/zipcoder/WCTest.java
@@ -8,4 +8,25 @@ import java.util.Arrays;
 
 public class WCTest {
 
+    @Test
+    public void testShort(){
+        WC test = new WC(WC.class.getResource("/superShort.txt").getFile());
+        test.textIterator();
+        test.sorted();
+        test.print();
+
+    }
+    @Test
+    public void testMedium(){
+        WC test = new WC(WC.class.getResource("/jabberwocky.txt").getFile());
+        test.textIterator();
+        test.sorted();
+    }
+    @Test
+    public void testLong(){
+        WC test = new WC(WC.class.getResource("/NineteenEightyFour.txt").getFile());
+        test.textIterator();
+        test.sorted();
+
+    }
 }
