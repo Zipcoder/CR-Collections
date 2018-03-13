512a21efddeb0a365237f786a635413d5615b8f5
Brian He <brianhe@zipcoders-MacBook-Pro-56.local>
2018-03-12 22:10:41 -0400

    finished

--- src/main/java/io/zipcoder/WC.java
+++ src/main/java/io/zipcoder/WC.java
@@ -2,22 +2,57 @@ package io.zipcoder;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.Iterator;
-import java.util.Scanner;
+import java.util.*;
+
+import static java.util.stream.Collectors.toMap;
 
 public class WC {
     private Iterator<String> si;
+    private LinkedHashMap<String, Integer> wordCounter;
 
     public WC(String fileName) {
         try {
             this.si = new Scanner(new FileReader(fileName));
         } catch (FileNotFoundException e) {
             System.out.println(fileName + " Does Not Exist");
-            System.exit(-1);
+            System.exit( - 1);
         }
     }
 
     public WC(Iterator<String> si) {
         this.si = si;
     }
+
+    public LinkedHashMap<String, Integer> storeWordsWithCount() {
+        wordCounter = new LinkedHashMap<>();
+        while(si.hasNext()) {
+            String word = si.next().replace(".", "").replace(",", "").toLowerCase();
+            if(!wordCounter.containsKey(word)) {
+                wordCounter.put(word, 1);
+                } else {
+                wordCounter.put(word, wordCounter.get(word) + 1);
+            }
+        }
+        return wordCounter;
+    }
+
+    public LinkedHashMap<String, Integer> sortWordCounter() {
+        LinkedHashMap<String, Integer> sortedMap = storeWordsWithCount().entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .collect(toMap(Map.Entry::getKey,
+                               Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+
+        return sortedMap;
+    }
+
+    public String printWordsAndCountInDescendingOrder() {
+        StringBuilder wordList = new StringBuilder();
+        LinkedHashMap<String, Integer> sortedMap = sortWordCounter();
+        for(Map.Entry<String, Integer> entries : sortedMap.entrySet()) {
+            wordList.append(entries.getKey() + " : " + entries.getValue().toString() + "\n");
+        }
+        return wordList.toString();
+    }
+
 }
--- src/test/java/io/zipcoder/WCTest.java
+++ src/test/java/io/zipcoder/WCTest.java
@@ -1,11 +1,37 @@
 package io.zipcoder;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 
 public class WCTest {
+    WC test;
+    WC test2;
 
+    @Before
+    public void setup() {
+        test = new WC(WC.class.getResource("/testText.txt").getFile());
+        test2 = new WC(WC.class.getResource("/textText2.txt").getFile());
+    }
+
+    @Test
+    public void wordCountTest() {
+        String expected = "hello : 2\nhi : 2\nmy : 1\nname : 1\nis : 1\nbrian : 1\n";
+        String actual = test.printWordsAndCountInDescendingOrder();
+
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void wordCountTest2() {
+        String expected = "hi : 4\nhello : 2\n";
+        String actual = test2.printWordsAndCountInDescendingOrder();
+
+        Assert.assertEquals(expected, actual);
+    }
 }
