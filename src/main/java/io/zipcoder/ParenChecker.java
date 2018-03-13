e3c9267320f709985f4d2e11f375fbbaa4143aef
Karousha Fennimore <karousha@gmail.com>
2018-03-12 20:20:16 -0400

    first 2 parts done

--- src/main/java/io/zipcoder/ParenChecker.java
+++ src/main/java/io/zipcoder/ParenChecker.java
@@ -1,4 +1,55 @@
 package io.zipcoder;
 
+import java.util.Stack;
+
 public class ParenChecker {
+    Stack<Character> stack = new Stack();
+
+    public boolean parenCheck(String string) {
+
+        for(int i = 0; i < string.length(); i++) {
+            char character = string.charAt(i);
+            //check if the stack is empty and the character is closing parenthesis (THIS IS BOUNDS)
+            if (stack.isEmpty() && character == ')') {
+                return false;
+                //if opening parenthesis add it to the stack
+            } else if (character == '(') {
+                stack.push(character);
+            }
+            //when you get a closing parenthesis, then remove the opening that matches
+            char last = stack.peek();
+            if (character == ')' && last =='(') {
+                stack.pop();
+            }
+        }
+        //if stack is empty then its true;
+        return stack.isEmpty();
+    }
+
+    public boolean openingHasClosing(String string) {
+        for (int i = 0; i < string.length(); i++) {
+            char current = string.charAt(i);
+            //check if the stack is empty and the character is closing parenthesis (THIS IS BOUNDS)
+
+            //if opening parenthesis add it to the stack
+            if (current == '(' || current == '{'
+                    || current == '[' || current == '<' || current == '"' || current == '\'') {
+                stack.push(current);
+            }
+            if ((current == ')' || current == '}' || current == ']' || current == '>') || current == '"' || current == '\'') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                //when you get a closing parenthesis, then remove the opening that matches
+                char last = stack.peek();
+                if (current == ')' && last == '(' || current == '}' && last == '{'
+                        || current == ']' && last == '[' || current == '>' && last == '<' || current == '"' && last == '"'
+                        || current == '\'' && last == '\'') {
+                    stack.pop();
+                }
+            }
+        }
+        //if stack is empty then its true;
+        return stack.isEmpty();
+    }
 }
--- src/test/java/io/zipcoder/ParenCheckerTest.java
+++ src/test/java/io/zipcoder/ParenCheckerTest.java
@@ -3,6 +3,96 @@ package io.zipcoder;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Stack;
+
 public class ParenCheckerTest {
+
+    @Test
+    public void parenCheckerFalseTest(){
+        //Given
+        ParenChecker parenTest = new ParenChecker();
+
+        //When
+        String hi = "(hello";
+
+        //Then
+        boolean actual = parenTest.parenCheck(hi);
+
+        Assert.assertFalse(actual);
+
+    }
+
+    @Test
+    public void parenCheckerTrueTest(){
+        //Given
+        ParenChecker parenTest = new ParenChecker();
+
+        //When
+        String hi = "(hello)";
+
+        //Then
+        boolean actual = parenTest.parenCheck(hi);
+
+        Assert.assertTrue(actual);
+
+    }
+
+    @Test
+    public void falseBracketTest(){
+        //Given
+        ParenChecker parenTest = new ParenChecker();
+
+        //When
+        String hi = "[hello";
+
+        //Then
+        boolean actual = parenTest.openingHasClosing(hi);
+
+        Assert.assertFalse(actual);
+
+    }
+    @Test
+    public void trueBracketTest(){
+        //Given
+        ParenChecker parenTest = new ParenChecker();
+
+        //When
+        String hi = "[he'l'lo]";
+
+        //Then
+        boolean actual = parenTest.openingHasClosing(hi);
+
+        Assert.assertTrue(actual);
+
+    }
+
+    @Test
+    public void trueCurlyBracesTest(){
+        //Given
+        ParenChecker parenTest = new ParenChecker();
+
+        //When
+        String hi = "{hello} [World]";
+
+        //Then
+        boolean actual = parenTest.openingHasClosing(hi);
+
+        Assert.assertTrue(actual);
+
+    }
+    @Test
+    public void falseCurlyBracesTest(){
+        //Given
+        ParenChecker parenTest = new ParenChecker();
+
+        //When
+        String hi = "{hello>";
+
+        //Then
+        boolean actual = parenTest.openingHasClosing(hi);
+
+        Assert.assertFalse(actual);
+
+    }
 
 }
