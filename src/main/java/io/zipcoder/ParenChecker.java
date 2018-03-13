db4f5b9d19158136e9d3dbd8ae658c86346dd8b1
Eric Cordell <ericcordell@zipcoders-MacBook-Pro-8.local>
2018-03-13 12:04:43 -0400

    Getting set up

--- src/main/java/io/zipcoder/ParenChecker.java
+++ src/main/java/io/zipcoder/ParenChecker.java
@@ -1,4 +1,38 @@
 package io.zipcoder;
 
 public class ParenChecker {
+
+    // Part 1
+    public boolean checkParentheses(String string) {
+        return false;
+    }
+
+
+    // Part 2
+    public boolean openAndClosedCharacters(String string) {
+        return false;
+    }
+
+
 }
+
+
+/*
+TC-Collections
+
+        Paren Checker
+
+        Part 1:
+        Create a class with a method that verifies all parens () are paired.
+        - HINT: Use a stack.
+        - Boolean
+        -
+
+        Part 2:
+        Now create a method that checks that all opening characters have a closing one.
+
+        Characters include () {} [] <> "" ''
+
+        - Boolean
+
+*/
--- src/test/java/io/zipcoder/ParenCheckerTest.java
+++ src/test/java/io/zipcoder/ParenCheckerTest.java
@@ -5,4 +5,57 @@ import org.junit.Test;
 
 public class ParenCheckerTest {
 
+
+    @Test
+    public void checkParenthesesTest1() {
+        // Given
+        ParenChecker testParentheses = new ParenChecker();
+
+        // When
+        String string1 = "Eagles are (Super Bowl) Champs";
+        boolean actual = testParentheses.checkParentheses(string1);
+
+        // Then
+        Assert.assertTrue(actual);
+    }
+
+
+    @Test
+    public void checkParenthesesTest2() {
+        // Given
+        ParenChecker testParentheses = new ParenChecker();
+
+        // When
+        String string2 = "Cowboys will not be (Super Bowl Champs anytime soon";
+        boolean actual = testParentheses.checkParentheses(string2);
+
+        // Then
+        Assert.assertFalse(actual);
+    }
+
+    @Test
+    public void openAndClosedCharactersTest1() {
+        // Given
+        ParenChecker testParentheses = new ParenChecker();
+
+        // When
+        String string1 = "Eagles will [repeat] as (Super Bowl) Champs";
+        boolean actual = testParentheses.openAndClosedCharacters(string1);
+
+        // Then
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void openAndClosedCharactersTest2() {
+        // Given
+        ParenChecker testParentheses = new ParenChecker();
+
+        // When
+        String string2 = "[Cowboys] will not be (Super Bowl Champs> {anytime soon";
+        boolean actual = testParentheses.openAndClosedCharacters(string2);
+
+        // Then
+        Assert.assertFalse(actual);
+    }
 }
