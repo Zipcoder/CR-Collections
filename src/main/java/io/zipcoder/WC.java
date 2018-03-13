1a0431750b9eb227fdbff53ca8b585124c018d82
Anthony Jordan <Kain235@gmail.com>
2018-03-13 13:02:59 -0400

    Part 3 working

--- src/main/java/io/zipcoder/WC.java
+++ src/main/java/io/zipcoder/WC.java
@@ -2,11 +2,11 @@ package io.zipcoder;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.Iterator;
-import java.util.Scanner;
+import java.util.*;
 
 public class WC {
     private Iterator<String> si;
+    private TreeMap<String, Integer> wordMap = new TreeMap();
 
     public WC(String fileName) {
         try {
@@ -19,5 +19,33 @@ public class WC {
 
     public WC(Iterator<String> si) {
         this.si = si;
+    }
+
+    public void wordCounter(){
+        String[] words;
+        for(String fileLine = si.next(); true; fileLine = si.next()) {
+            words = fileLine.split(" ");
+            for (String word: words) {
+                if (!wordMap.containsKey(word.toLowerCase())){
+                    wordMap.put(word.toLowerCase(), 1);
+                } else {
+                    wordMap.put(word.toLowerCase(), wordMap.get(word)+ 1);
+                }
+            }
+            if (!si.hasNext()){
+                break;
+            }
+        }
+        printAllResults();
+    }
+
+    public TreeMap<String, Integer> getWordMap() {
+        return wordMap;
+    }
+
+    public void printAllResults(){
+        for (String key: wordMap.keySet()) {
+            System.out.println(key + " " + wordMap.get(key));
+        }
     }
 }
