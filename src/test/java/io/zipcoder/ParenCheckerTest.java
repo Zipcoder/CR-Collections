8c09cda5b8a658494d81354f6e199f724d740ec3
mbowe4 <mlbowe@gmail.com>
2018-03-12 22:10:41 -0400

    parts 1 & 2 complete

--- src/test/java/io/zipcoder/ParenCheckerTest.java
+++ src/test/java/io/zipcoder/ParenCheckerTest.java
@@ -5,4 +5,61 @@ import org.junit.Test;
 
 public class ParenCheckerTest {
 
+    @Test
+    public void isPairedTest1() {
+        ParenChecker parenChecker = new ParenChecker();
+        String str = "()()()()";
+
+        Assert.assertTrue(parenChecker.isPaired(str));
+    }
+
+    @Test
+    public void isPairedTest2() {
+        ParenChecker parenChecker = new ParenChecker();
+        String str = ")()()()()((";
+
+        Assert.assertFalse(parenChecker.isPaired(str));
+    }
+
+    @Test
+    public void isPairedTest3() {
+        ParenChecker parenChecker = new ParenChecker();
+        String str = ")((()";
+
+        Assert.assertFalse(parenChecker.isPaired(str));
+    }
+
+    @Test
+    public void hasClosingTest1() {
+        ParenChecker parenChecker = new ParenChecker();
+        String str = "<<>>(){}[]";
+
+        Assert.assertTrue(parenChecker.hasClosing(str));
+    }
+
+    @Test
+    public void hasClosingClosingTest2() {
+        ParenChecker parenChecker = new ParenChecker();
+        String str = "\"\"";
+
+        Assert.assertTrue(parenChecker.hasClosing(str));
+    }
+
+    @Test
+    public void hasClosingClosingTest3() {
+        ParenChecker parenChecker = new ParenChecker();
+        String str = "\'\'\'";
+
+        Assert.assertFalse(parenChecker.hasClosing(str));
+    }
+
+    @Test
+    public void hasPairTest1() {
+        ParenChecker parenChecker = new ParenChecker();
+        String str = "()()()()";
+
+        Assert.assertTrue(parenChecker.hasPair(str, '(', ')'));
+    }
+
+
 }
