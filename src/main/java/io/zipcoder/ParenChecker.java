e8aa9773dce5df14d4175d517b8eadbc6108a230
Haysel Santiago <hayselsantiago@zipcoders-MacBook-Pro-67.local>
2018-03-13 08:42:32 -0400

    Part 1, 2 & Tests Passing

--- src/main/java/io/zipcoder/ParenChecker.java
+++ src/main/java/io/zipcoder/ParenChecker.java
@@ -1,4 +1,92 @@
 package io.zipcoder;
 
+//import java.util.HashMap;
+//import java.util.Map;
+import java.util.Stack;
+
 public class ParenChecker {
+
+    Stack<Character> parenStack = new Stack();
+
+    //going to pass in the chars we are looking for contained within a String ''
+    public boolean parenChecker(String string) {
+        for(int i = 0; i < string.length(); i++) {
+            if(string.charAt(i) == ')' && parenStack.empty()){
+                break;
+            }
+            if (string.charAt(i) == '(') {
+                parenStack.push(string.charAt(i));
+            }
+            if(string.charAt(i) ==')'){
+                parenStack.pop();
+            }
+        }
+        return parenStack.empty();
+    }
+
+    public boolean pairOfParenthesies(String string) {
+        for (int i =0; i < string.length(); i++) {
+            char currentCharacter = string.charAt(i);
+            if (currentCharacter == '[' || currentCharacter == '"' || currentCharacter == '<' || currentCharacter == '\'' || currentCharacter == '{') {
+                parenStack.push(currentCharacter);
+            }
+            if (currentCharacter == ']' || currentCharacter == '"' || currentCharacter == '>' || currentCharacter == '\'' || currentCharacter == '}' ) {
+                if (parenStack.isEmpty()) {
+                    return false;
+                }
+                // look at the object at the top of the stack but don't remove, PEEK
+                char finalCharacter = parenStack.peek();
+                if (currentCharacter == ']' && finalCharacter == '[' || currentCharacter == '"' && finalCharacter == '"' || currentCharacter == '<' && finalCharacter =='>' || currentCharacter == '\'' && finalCharacter == '\'' || currentCharacter == '}' && finalCharacter == '{') {
+                    parenStack.pop();
+                }
+            }
+        }
+        return parenStack.isEmpty();
+    }
 }
+
+    //check if characters are balanced//
+//
+//    private static boolean isBalanced(String str) {
+//        Map<Character, Character> bracketPairs = new HashMap<Character,  Character>(){
+//            {
+//                put('(', ')');
+//                put('{', '}');
+//                put('[', ']');
+//                put('<', '>');
+//            }
+//        };
+//
+//        //in order to be balanced, there ought to be an even number of chars
+//        if(str.length() % 2 !=0) {
+//            return false;
+//        }
+//        //stores opening braces in this case
+//        Stack<Character> halfBraces = new Stack<>();
+//
+//        for(int i = 0; i < str.length(); i++) {
+//            //if characters (bracket) is opening, then store into stack
+//            if(bracketPairs.containsKey(str.charAt(i))){
+//                halfBraces.push(str.charAt(i));
+//            }
+//
+//            //if closing bracket is not equal to top of stack or if stack is empty, return false
+//            else if(halfBraces.isEmpty() || bracketPairs.get(halfBraces.pop()) !=str.charAt(i)) {
+//                return false;
+//            }
+//        }
+//        return halfBraces.isEmpty() ? true : false;
+//    }
+//
+//}
+
+
+//    public static boolean checkParenthesies(String input) {
+//        Stack<Character> stack = new Stack<Character>();
+//        for (int i = 0; i < input.length(); i++) {
+//            char c = input.charAt(i);
+//            if (c == '[' || c == '(' || c == '{') {
+//                stack.push(c);
+//            }
+//        }
+//    }
--- src/test/java/io/zipcoder/ParenCheckerTest.java
+++ src/test/java/io/zipcoder/ParenCheckerTest.java
@@ -3,6 +3,63 @@ package io.zipcoder;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Stack;
+
+
 public class ParenCheckerTest {
 
+    @Test
+    public void parenthesisCheckerTest() {
+        //Given
+        ParenChecker parenthesisTest = new ParenChecker();
+
+        //When
+        String testString = "()";
+        boolean actual = parenthesisTest.parenChecker(testString);
+
+        //Then
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void parenthesisCheckerTest2() {
+        //Given
+        ParenChecker parenthesisTest = new ParenChecker();
+
+        //When
+        String testString = "(";
+        boolean actual = parenthesisTest.parenChecker(testString);
+
+        //Then
+        Assert.assertFalse(actual);
+    }
+
+    @Test
+    public void parenthesisCheckerTest3() {
+        //Given
+        ParenChecker parenthesisTest = new ParenChecker();
+
+        //When
+        String testString2 = "()<>";
+        boolean actual = parenthesisTest.pairOfParenthesies(testString2);
+
+        //Then
+        Assert.assertFalse(actual);
+    }
+
+    @Test
+    public void parenthesisCheckerTest4() {
+        //Given
+        ParenChecker parenthesisTest = new ParenChecker();
+
+        //When
+        String testString = "()<>''";
+        boolean actual = parenthesisTest.pairOfParenthesies(testString);
+
+        //Then
+        Assert.assertFalse(actual);
+    }
 }
+// Single (
+// more than one set () <>
+// more than two sets () <> ''
