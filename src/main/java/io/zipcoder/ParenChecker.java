a71c12ce07de40c7eb2bbdc5a553068876bb0833
Daniel Horowitz <danielhorowitz@zipcoders-MacBook-Pro-80.local>
2018-03-13 05:20:15 -0400

    cant pass the multiple test case yet

--- src/main/java/io/zipcoder/ParenChecker.java
+++ src/main/java/io/zipcoder/ParenChecker.java
@@ -1,4 +1,62 @@
 package io.zipcoder;
 
+import java.util.Stack;
+
 public class ParenChecker {
+
+    Stack<Character> checker = new Stack();
+
+    public boolean parenCheck(String check) {
+
+        for (int i = 0; i < check.length(); i++) {
+            if (check.charAt(i) == ')' && checker.isEmpty())
+                return false;
+            if (check.charAt(i) == '(') {
+                checker.push(check.charAt(i));
+            }
+            if (check.charAt(i) == ')' ) {
+                checker.pop();
+            }
+        }
+        return checker.isEmpty();
+    }
+
+    public boolean allCheck(String check) {
+//        Pair input = new Pair('&', '&');
+
+        for (int i = 0; i < check.length(); i++) {
+            char current = check.charAt(i);
+//                if(check.charAt(i) == input.getOpen())
+//                    checker.push(check.charAt(i));
+//                char top = checker.peek();
+//                if(check.charAt(i) == input.getClose(top) && top == input.getOpen()) {
+//                    checker.pop();
+//            }
+
+
+
+            if ((current == ')' || current == '}' || current == ']' || current == '>') && checker.isEmpty()) {
+                return false;
+            }
+            if (current == '(' || current == '{' || current == '[' ||
+                    current == '<' || current == '\'' || current == '"') {
+                checker.push(current);
+            }
+            char top = checker.peek();
+            if (current == ')' && top == '(' || current == '}' && top == '{' ||
+                    current == ']' && top == '[' || current == '>' && top == '<' ||
+                    current == '\'' && top == '\'' || current == '"' && top == '"') {
+                checker.pop();
+            }
+      }
+        return checker.isEmpty();
+    }
+
 }
+
+
+
+
+
+
+
--- src/test/java/io/zipcoder/ParenCheckerTest.java
+++ src/test/java/io/zipcoder/ParenCheckerTest.java
@@ -1,8 +1,91 @@
 package io.zipcoder;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Stack;
 
 public class ParenCheckerTest {
 
+    @Test
+    public void parenCheckerTest() {
+        //Given
+        ParenChecker parenChecker = new ParenChecker();
+
+
+        String string1 = "I love (Luke)";
+        String string2 = "I love (Cars)";
+        String string3 = "I love coding)";
+
+
+        boolean actual = parenChecker.parenCheck(string3);
+
+        Assert.assertFalse(actual);
+
+
+    }
+
+    @Test
+    public void parenCheckerTest1() {
+        //Given
+        ParenChecker parenChecker = new ParenChecker();
+
+
+        String string1 = "I love (Luke)";
+        String string2 = "I love (Cars)";
+        String string3 = "I love coding)";
+
+
+        Assert.assertTrue(parenChecker.parenCheck(string1));
+
+    }
+
+    @Test
+    public void parenCheckerTest2() {
+        //Given
+        ParenChecker parenChecker = new ParenChecker();
+
+
+        String string1 = "I love (Luke)";
+        String string2 = "I love (Cars)";
+        String string3 = "I \"love\" { { [c] }}o\'d\'ing)";
+
+
+        boolean actual = parenChecker.allCheck(string3);
+
+        Assert.assertFalse(actual);
+
+
+    }
+
+    @Test
+    public void parenCheckerTest4() {
+        //Given
+        ParenChecker parenChecker = new ParenChecker();
+
+
+        String string1 = "I love (Luke)";
+        String string2 = "I love (Cars)";
+        String string3 = "I (\"love\" { { [c] }}oding)\"";
+
+
+        Assert.assertTrue(parenChecker.allCheck(string3));
+
+    }
+
+    @Test
+    public void parenCheckerTest5() {
+        //Given
+        ParenChecker parenChecker = new ParenChecker();
+
+
+        String string1 = "I love (Luke)";
+        String string2 = "I {love >Cars";
+        String string3 = "I (\"love\" { { [c] }}oding)\"";
+
+
+        Assert.assertFalse(parenChecker.allCheck(string2));
+
+    }
 }
