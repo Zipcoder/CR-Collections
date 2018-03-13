aff5b172ca722f505d7c3fdaca76e90cb2437972
April Rivera <aprilrivera@zipcoders-MacBook-Pro-2.local>
2018-03-13 14:35:42 -0400

    still working

--- src/main/java/io/zipcoder/WC.java
+++ src/main/java/io/zipcoder/WC.java
@@ -2,11 +2,13 @@ package io.zipcoder;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.Iterator;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class WC {
     private Iterator<String> si;
+
+    Map<String, Integer> wordMap = new HashMap<>();
 
     public WC(String fileName) {
         try {
@@ -19,5 +21,36 @@ public class WC {
 
     public WC(Iterator<String> si) {
         this.si = si;
+    }
+    private void splitFile(){
+        ArrayList<String> words = new ArrayList<>();
+        String eachWord;
+        while(si.hasNext()){
+            eachWord = si.next();
+            words.add(eachWord);
+        }
+    }
+    private void wordCount(){
+        while(si.hasNext()){
+            String word = si.next().toLowerCase();
+            Integer freq = wordMap.get(word);
+            if(freq == null){
+                wordMap.put(word, 1);
+            }else wordMap.put(word, freq+1);
+        }
+
+    }
+    private Map<String, Integer> sortedMap(){
+        Map<String, Integer>
+        myMap = wordMap
+                .entrySet()
+                .stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2, LinkedHashMap::new));
+
+        for(Map.Entry entry: myMap){
+            System.out.println(entry);
+        }
+        return myMap;
     }
 }
--- src/test/java/io/zipcoder/WCTest.java
+++ src/test/java/io/zipcoder/WCTest.java
@@ -8,4 +8,31 @@ import java.util.Arrays;
 
 public class WCTest {
 
+    @Test
+    public void splitFileTest(){
+        //Given
+
+        //When
+
+        //Then
+
+    }
+    @Test
+    public void wordCountTest(){
+        //Given
+
+        //When
+        WC.class.getResource("/someFileText").getFile();
+        //Then
+
+    }
+    @Test
+    public void sortedMapTest(){
+        //Given
+
+        //When
+
+        //Then
+
+    }
 }
