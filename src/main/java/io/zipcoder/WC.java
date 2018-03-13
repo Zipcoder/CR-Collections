4b29a1d551054cfc4fa99531b79b982f0eb2a549
Karousha Fennimore <karousha@gmail.com>
2018-03-13 15:40:23 -0400

    completed

--- src/main/java/io/zipcoder/WC.java
+++ src/main/java/io/zipcoder/WC.java
@@ -2,18 +2,24 @@ package io.zipcoder;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.Iterator;
-import java.util.Scanner;
-import java.util.SortedMap;
+import java.util.*;
+
+import static java.util.stream.Collectors.toMap;
 
 public class WC {
 
     private Iterator<String> si;
+    private Map<String, Integer> listOfWords = new LinkedHashMap<>();
 
+
+    public static void main(String[] args) {
+        WC wc = new WC("/Users/karoushafennimore/Dev/EncapsulativeCharacters/src/main/resources/WarPeace.txt");
+        wc.printMap();
+    }
 
     public WC(String fileName) {
         try {
-            this.si = new Scanner(new FileReader("text.txt"));
+            this.si = new Scanner(new FileReader(fileName));
         } catch (FileNotFoundException e) {
             System.out.println(fileName + " Does Not Exist");
             System.exit(-1);
@@ -25,78 +31,51 @@ public class WC {
         this.si = si;
     }
 
+//
+//    public int wordCount(){
+//    int count = 0;
+//
+//    while(si.hasNext()) {
+//        String[] words = si.next().toLowerCase().split(" ");
+//        count += words.length;
+//    }
+//    return count;
+//
+//    }
 
-    public int wordCount(){
-    int count = 0;
 
-    while(si.hasNext()) {
-        String[] words = si.next().split(" ");
-        count += words.length;
+    public Map<String, Integer> uniqueWordCount() {
+        int count = 0;
+
+        while (si.hasNext()) {
+            count++;
+            String word = si.next().toLowerCase().replaceAll("[^a-z]", "" );
+
+                if (listOfWords.containsKey(word)) {
+                    listOfWords.put(word, listOfWords.get(word) +1);
+                } else {
+                    listOfWords.put(word, 1);
+                }
+        } listOfWords.put("Count For Entire File", count);
+        return listOfWords;
     }
-    return count;
 
+
+    public Map<String, Integer> returnValueDesc() {
+        Map<String, Integer> sorted =
+                uniqueWordCount().entrySet().stream()
+                        .sorted(Map.Entry.<String, Integer> comparingByValue().reversed())
+                        .collect(toMap(
+                                Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+        return sorted;
     }
 
-//    public SortedMap<String, Integer> wordCount() {
-//        while(si.hasNext()) {
-//            si.next().replaceAll("\\W", " ").toUpperCase();
-//            uniqueWordMap =
-//            for (String uniqueWord : "text.txt") {
-//            {
-//                //if word is already present in wordCountMap, updating its count
-//                if(si.containsKey(uniqueWord))
-//                {
-//                    wordCountMap.put(uniqueWord, wordCountMap.get(uniqueWord)+1);
-//                }
-//
-//                //otherwise inserting the word as key and 1 as its value
-//                else
-//                {
-//                    wordCountMap.put(uniqueWord, 1);
-//                }
-//            }
-//
-//            //Reading next line into currentLine
-//
-//            currentLine = reader.readLine();
-//        }
-//
-//        //Getting the most repeated word and its occurrence
-//
-//        String mostRepeatedWord = null;
-//
-//        int count = 0;
-//
-//        Set<Entry<String, Integer>> entrySet = wordCountMap.entrySet();
-//
-//        for (Entry<String, Integer> entry : entrySet)
-//        {
-//            if(entry.getValue() > count)
-//            {
-//                mostRepeatedWord = entry.getKey();
-//
-//                count = entry.getValue();
-//            }
-//        }
-//
-//        System.out.println("The most repeated word in input file is : "+mostRepeatedWord);
-//
-//        System.out.println("Number Of Occurrences : "+count);
-//    }
-//        catch (IOException e)
-//    {
-//        e.printStackTrace();
-//    }
-//        finally
-//    {
-//        try
-//        {
-//            reader.close();           //Closing the reader
-//        }
-//        catch (IOException e)
-//        {
-//            e.printStackTrace();
-//        }
-//    }
-
+    public String printMap(){
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<String, Integer> words: returnValueDesc().entrySet()) {
+            sb.append("Word: " + words.getKey() + "\n" + "Appears: " + words.getValue() + " times \n" + "_____________\n");
+        }
+        System.out.println(sb.toString());
+        return sb.toString();
+    }
 }
