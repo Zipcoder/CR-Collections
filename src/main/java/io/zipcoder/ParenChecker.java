46ccfcb03765552261a6c4495e43c4b90fff436c
Katrice Williams-Dredden <katricewilliams-dredden@zipcoders-MacBook-Pro-4.local>
2018-03-13 13:09:39 -0400

    part2

--- src/main/java/io/zipcoder/ParenChecker.java
+++ src/main/java/io/zipcoder/ParenChecker.java
@@ -1,4 +1,52 @@
 package io.zipcoder;
 
+
+import java.util.Stack;
+
+//1.Create a class with a method that verifies all parens () are paired. HINT: Use a stack.
 public class ParenChecker {
+
+    Stack<Character> stack = new Stack<Character>();
+
+    public ParenChecker(){
+
+    }
+
+    public boolean parenChecker(String value){
+
+        for(int i = 0; i < value.length(); i++){
+            char c = value.charAt(i);
+            if(c == '('){
+                stack.push(c);
+            } else if (c == ')'){
+                if(stack.isEmpty()){
+                    return false;
+                }
+                stack.pop();
+            }
+
+        }
+        return stack.isEmpty();
+    }
+
+//TODO need to add " and ' to method maybe in a seperate if statment
+    public boolean isMatchChecker(String value){
+        for(int i = 0; i<value.length();i++){
+            char c = value.charAt(i);
+            //question about last two comparisons
+            if(c == '(' || c == '[' || c== '<'|| c == '{'){
+                stack.push(c);
+            }else if(c==')'){
+                return false;
+            }else if(c==')' || c==']' || c== '>' || c == '}'){
+
+            }
+
+
+        }
+    }
+
+
 }
+
+
--- src/test/java/io/zipcoder/ParenCheckerTest.java
+++ src/test/java/io/zipcoder/ParenCheckerTest.java
@@ -3,6 +3,17 @@ package io.zipcoder;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class ParenCheckerTest {
 
+    @Test
+    public void TestParenChecker(){
+        //Given
+        ParenChecker parenChecker = new ParenChecker();
+        String value = "I rock (rough) and stuff with my (afro) puffs!";
+        //When
+        boolean pairParen = parenChecker.parenChecker(value);
+        Assert.assertFalse(pairParen);
+    }
 }
