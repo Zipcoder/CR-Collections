2500744d480e83598aecce9d95fa80ff296a5e57
CWinarski <Winarski70@yahoo.com>
2018-03-13 19:37:19 -0400

    Finished Lab

--- src/main/java/io/zipcoder/WC.java
+++ src/main/java/io/zipcoder/WC.java
@@ -2,10 +2,13 @@ package io.zipcoder;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.Iterator;
-import java.util.Scanner;
+import java.util.*;
+
+import static java.util.stream.Collectors.toMap;
 
 public class WC {
+
+    Map<String, Integer> wordsAndTheirCounts = new LinkedHashMap<String, Integer>();
     private Iterator<String> si;
 
     public WC(String fileName) {
@@ -20,4 +23,53 @@ public class WC {
     public WC(Iterator<String> si) {
         this.si = si;
     }
+
+    //Sort values into map method
+    public Map<String, Integer> countNumberOfTimesWordsOccur(){
+        Integer wordCount = 0;
+        while(si.hasNext()){
+            wordCount++;
+            String word = si.next().toLowerCase().replaceAll("[^a-z]", "");
+            if(wordsAndTheirCounts.containsKey(word)){
+                    wordsAndTheirCounts.put(word, wordsAndTheirCounts.get(word) +1);
+            } else{
+                wordsAndTheirCounts.put(word, 1);
+            }
+        }
+        wordsAndTheirCounts.put("Total Words", wordCount);
+        return wordsAndTheirCounts;
+    }
+
+    //Order map in descending order method
+    public Map<String, Integer> sortMapIntoDescendingOrder() {
+        List<Map.Entry<String, Integer>> toSort = new ArrayList<>();
+        for (Map.Entry<String, Integer> stringIntegerEntry : countNumberOfTimesWordsOccur().entrySet()) {
+            toSort.add(stringIntegerEntry);
+        }
+        toSort.sort(Map.Entry.<String, Integer>
+                comparingByValue().reversed().thenComparing(Map.Entry.comparingByKey()));
+        Map<String, Integer> wordsAndTheirCountsDesc = new LinkedHashMap<>();
+        for (Map.Entry<String, Integer> stringIntegerEntry : toSort) {
+            wordsAndTheirCountsDesc.putIfAbsent(stringIntegerEntry.getKey(), stringIntegerEntry.getValue());
+        }
+        return wordsAndTheirCountsDesc;
+    }
+
+    //Print map method
+    public String printMap(){
+        StringBuilder printedList = new StringBuilder();
+        for (Map.Entry<String, Integer> entry : sortMapIntoDescendingOrder().entrySet()){
+            printedList.append(entry.getKey());
+            printedList.append(":");
+            printedList.append(" appears ");
+            printedList.append(entry.getValue());
+            printedList.append(" times\n");
+        }
+        return printedList.toString();
+    }
+
+
+/// ^ reads like does not equal
+// ::
+
 }
--- src/test/java/io/zipcoder/WCTest.java
+++ src/test/java/io/zipcoder/WCTest.java
@@ -3,9 +3,20 @@ package io.zipcoder;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class WCTest {
+     WC testWC = new WC( WC.class.getResource("/testFile.txt").getFile());
+    @Test
+    public void checkIfPrints(){
+        String expected = "Total Words: appears 11 times\n" + "lab: appears 2 times\n" + "the: appears 2 times\n" + "done: appears 1 times\n"  +
+                "getting: appears 1 times\n" + "help: appears 1 times\n" + "interesting: appears 1 times\n"
+                + "is: appears 1 times\n" + "me: appears 1 times\n" + "will: appears 1 times\n";
+        String actual = testWC.printMap();
+        Assert.assertEquals(expected, actual);
+    }
 
+    @Test
+    public void SanctuaryTest(){
+        WC bookWC = new WC(WC.class.getResource("/someTextFile.txt").getFile());
+        System.out.println(bookWC.printMap());
+    }
 }
