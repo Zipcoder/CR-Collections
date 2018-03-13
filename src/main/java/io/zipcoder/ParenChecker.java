be1486e81ba7ff3fc2d2ff2c730431d5378a0847
April Rivera <aprilrivera@zipcoders-MBP-2.fios-router.home>
2018-03-12 23:54:45 -0400

    Part 1 and 2

--- src/main/java/io/zipcoder/ParenChecker.java
+++ src/main/java/io/zipcoder/ParenChecker.java
@@ -1,4 +1,50 @@
 package io.zipcoder;
 
+import java.util.Stack;
+
 public class ParenChecker {
+    Stack<Character> stack = new Stack<Character>();
+
+    public ParenChecker() {
+    }
+
+    public boolean parenChecker(String s){
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(c == '(' ){
+                stack.push(c);
+            }else if(c == ')'){
+                if(stack.isEmpty() || stack.pop() != '('){
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+    public boolean pairChecker(String s){
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(c == '(' || c == '{' || c == '[' || c == '<' || c == ' ' || c == '"') {
+                stack.push(c);
+            }else if(c == ')') {
+                if (stack.isEmpty() || stack.pop() != '(') return false;
+            }
+            else if(c == '}'){
+                if(stack.isEmpty() || stack.pop() != '{') return false;
+            }
+            else if(c == ']'){
+                if(stack.isEmpty() || stack.pop() != '[') return false;
+            }
+            else if(c == '>'){
+                if(stack.isEmpty() || stack.pop() != '<') return false;
+            }
+            else if(c == '\''){
+                if(stack.isEmpty() || stack.pop() != '\'') return false;
+            }
+            else if(c == '\"'){
+                if(stack.isEmpty() || stack.pop() != '\"') return false;
+            }
+        }
+        return stack.isEmpty();
+    }
 }
--- src/test/java/io/zipcoder/ParenCheckerTest.java
+++ src/test/java/io/zipcoder/ParenCheckerTest.java
@@ -5,4 +5,54 @@ import org.junit.Test;
 
 public class ParenCheckerTest {
 
+    @Test
+    public void parenCheckerTest(){
+        //Given
+        ParenChecker parenChecker = new ParenChecker();
+        String s = ")()(";
+        //When
+        boolean allPair = parenChecker.parenChecker(s);
+        //Then
+        Assert.assertFalse(allPair);
+    }
+    @Test
+    public void parenCheckerTest2(){
+        //Given
+        ParenChecker parenChecker = new ParenChecker();
+        String s = "()()";
+        //When
+        boolean hasAllPair = parenChecker.parenChecker(s);
+        //Then
+        Assert.assertTrue(hasAllPair);
+    }
+    @Test
+    public void pairCheckerTest(){
+        //Given
+        ParenChecker parenChecker = new ParenChecker();
+        String s = "{true}";
+        //When
+        boolean hasPair = parenChecker.pairChecker(s);
+        //Then
+        Assert.assertTrue(hasPair);
+    }
+    @Test
+    public void pairCheckTest2(){
+        //Given
+        ParenChecker parenChecker = new ParenChecker();
+        String s = "<False pacman mouth<";
+        //When
+        boolean hasPair = parenChecker.pairChecker(s);
+        //Then
+        Assert.assertFalse(hasPair);
+    }
+    @Test
+    public void pairCheckerTest3(){
+        //Given
+        ParenChecker parenChecker = new ParenChecker();
+        String s = "[size]";
+        //When
+        boolean hasPair = parenChecker.pairChecker(s);
+        //Then
+        Assert.assertTrue(hasPair);
+    }
 }
