f21e6629fd4675df93ad689583389441e5c4584a
ghost1497 <rfrankie04@gmail.com>
2018-03-13 16:24:01 -0400

    project done

--- src/main/java/io/zipcoder/ParenChecker.java
+++ src/main/java/io/zipcoder/ParenChecker.java
@@ -1,4 +1,83 @@
 package io.zipcoder;
 
+import java.util.Stack;
+
 public class ParenChecker {
+    private Stack charStack;
+
+    public ParenChecker() {
+        this.charStack = new Stack<Character>();
+    }
+
+    public void add(String input) {
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) != 32) {
+                charStack.add(input.charAt(i));
+            }
+        }
+    }
+
+    public String getCharString() {
+        StringBuilder result = new StringBuilder();
+        result.append(charStack);
+        return result.toString();
+    }
+
+    public boolean isOpen() {
+        boolean result = true;
+        int openCount = 0;
+        int quoteCount = 0;
+        for (int i = 0; i < charStack.size(); i++) {
+            char input = charStack.get(i).toString().charAt(0);
+            switch (input) {
+                case '(':
+                    openCount++;
+                    break;
+                case ')':
+                    openCount--;
+                    break;
+                case '{':
+                    openCount++;
+                    break;
+                case '}':
+                    openCount--;
+                    break;
+                case '[':
+                    openCount++;
+                    break;
+                case ']':
+                    openCount--;
+                    break;
+                case '<':
+                    openCount++;
+                    break;
+                case '>':
+                    openCount--;
+                    break;
+                case 34:
+                    if (quoteCount == 1) {
+                        openCount--;
+                        quoteCount--;
+                        break;
+                    }
+                    openCount++;
+                    quoteCount++;
+                    break;
+                case 39:
+                    if (quoteCount == 1) {
+                        openCount--;
+                        quoteCount--;
+                        break;
+                    }
+                    openCount++;
+                    quoteCount++;
+                    break;
+            }
+        }
+        if (openCount == 0) {
+            result = false;
+        }
+        return result;
+    }
+
 }
--- src/main/java/io/zipcoder/WC.java
+++ src/main/java/io/zipcoder/WC.java
@@ -2,11 +2,11 @@ package io.zipcoder;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.Iterator;
-import java.util.Scanner;
+import java.util.*;
 
 public class WC {
     private Iterator<String> si;
+    private Map<String, Integer> wordsAndCounts = new LinkedHashMap<String, Integer>();
 
     public WC(String fileName) {
         try {
@@ -20,4 +20,40 @@ public class WC {
     public WC(Iterator<String> si) {
         this.si = si;
     }
+
+    public WC() {}
+
+    public void program(){
+        ArrayList<String> words = new ArrayList<String>();
+        do{
+            words.add(si.next());
+        }while(si.hasNext() == true);
+        fillTreeMap(words);
+        printInfo();
+    }
+
+    private void printInfo() {
+        for(Map.Entry entry : wordsAndCounts.entrySet()){
+            System.out.println(String.format("Word: %-20s  |  Occurrences: %s", entry.getKey().toString(), entry.getValue().toString()));
+        }
+    }
+
+    public void fillTreeMap(ArrayList words){
+        Collections.sort(words, Collections.reverseOrder());
+        for(Object element : words){
+            int occ = getOccurrences(element, words);
+            wordsAndCounts.put(element.toString(), occ);
+        }
+    }
+
+    public int getOccurrences(Object element, ArrayList words){
+        int wordCount = 0;
+        for(Object word : words){
+            if(word.equals(element)){
+                wordCount++;
+            }
+        }
+        return wordCount;
+    }
+
 }
