1bb908c285eccdf408d61c2efcf372f0a8b8bd1c
Eric Cordell <ericcordell@zipcoders-MacBook-Pro-8.local>
2018-03-13 23:20:58 -0400

    up to Part 2

--- src/main/java/io/zipcoder/ParenChecker.java
+++ src/main/java/io/zipcoder/ParenChecker.java
@@ -1,19 +1,65 @@
 package io.zipcoder;
 
+import java.util.Stack;
+
 public class ParenChecker {
 
-    // Part 1
+
+    Stack<Character> stackOfParentheses = new Stack();
+
+    // Part 1 - gonna pass in the chars we are looking for contained within a string ' '
     public boolean checkParentheses(String string) {
-        return false;
+
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == ')' && stackOfParentheses.empty()) {
+                break;
+            }
+            if (string.charAt(i) == '(') {
+                stackOfParentheses.push(string.charAt(i));  // push - element on top of stack
+            }
+            if (string.charAt(i) == ')') {
+                stackOfParentheses.pop();   // pop - removes & returns the top element of stack
+            }
+        }
+        return stackOfParentheses.empty();  // true if nothing is on top of stack
     }
 
 
     // Part 2
     public boolean openAndClosedCharacters(String string) {
-        return false;
+
+        for (int i = 0; i < string.length(); i++) {
+            char currentChar = string.charAt(i);
+
+            // checking opening characters
+            if (currentChar == '(' || currentChar == '{'
+                    || currentChar == '[' || currentChar == '<'
+                    || currentChar == '"' || currentChar == '\'') {
+                stackOfParentheses.push(currentChar);
+            }
+            // checking closing characters
+            if (currentChar == ')' || currentChar == '}'
+                    || currentChar == ']' || currentChar == '>'
+                    || currentChar == '"' || currentChar == '\'') {
+
+                if (stackOfParentheses.isEmpty()) {
+                    return false;
+                }
+                    // checking both opening and closing characters
+                char lastChar = stackOfParentheses.peek(); // peek- returns element on top of stack w/o removing it
+                if (currentChar == ')' && lastChar == '('
+                        || currentChar == '}' && lastChar == '{'
+                        || currentChar == ']' && lastChar == '['
+                        || currentChar == '>' && lastChar == '<'
+                        || currentChar == '"' && lastChar == '"'
+                        || currentChar == '\'' && lastChar == '\'') {
+                    stackOfParentheses.pop();
+                }
+            }
+        }
+
+        return stackOfParentheses.isEmpty();
     }
-
-
 }
 
 
