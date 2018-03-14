dbdf104c4aa67b5258a2efbb5e7ef15ac3566220
Keith Brinker <keithbrinker@zipcoders-MacBook-Pro-2.local>
2018-03-14 11:04:20 -0400

    finished

--- src/main/java/io/zipcoder/WC.java
+++ src/main/java/io/zipcoder/WC.java
@@ -2,13 +2,14 @@ package io.zipcoder;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.Iterator;
-import java.util.Scanner;
-import java.util.TreeMap;
+import java.util.*;
+
+import static java.util.stream.Collectors.toMap;
+
 
 public class WC {
     private Iterator<String> si;
-    public TreeMap<String, Integer> wordMap = new TreeMap<String, Integer>();
+    public Map<String, Integer> wordMap = new LinkedHashMap<String, Integer>();
 
 
     public WC(String fileName) {
@@ -24,32 +25,28 @@ public class WC {
         this.si = si;
     }
 
-    public void addWord(String word, Integer count) {
-        wordMap.put(word, count++);
-    }
+    public Map<String, Integer> wordCountStorage() {
 
-    public void getValue(String word, Integer count) {
-
-    }
-
-
-    public TreeMap wordCountStorage() {
-
-        TreeMap<String, Integer> wordMap = new TreeMap<String, Integer>();
         while (si.hasNext()) {
-            String current = si.next().toLowerCase().replaceAll("[^A-Za-z0-9]", "");
+            String current = si.next().toLowerCase().replaceAll("[^a-z0-9]", "");
             if (!wordMap.containsKey(current)) {
                 wordMap.put(current, 1);
-            }
-            else {
-                Integer currentCount = wordMap.get(current);
-                wordMap.put(current, currentCount+1);
+            } else {
+                wordMap.put(current, wordMap.get(current) + 1);
             }
         }
         return wordMap;
     }
 
+    public Map<String, Integer> sortInDescOrderByValue() {
+        Map<String, Integer> sortByDesc = wordCountStorage().entrySet()
+                .stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                .collect(toMap(Map.Entry::getKey,
+                        Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
 
+        return sortByDesc;
+    }
 }
 
 
