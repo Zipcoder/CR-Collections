01b1cba03c74a646addb9acd71f84a1b16b74d1e
Amy Gill <amygill@MacBook-Pro-2.local>
2018-03-14 10:41:25 -0400

    hashmap but should probably be a treemap

--- src/test/java/io/zipcoder/WCTest.java
+++ src/test/java/io/zipcoder/WCTest.java
@@ -1,11 +1,29 @@
 package io.zipcoder;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.TreeMap;
 
 public class WCTest {
+
+    WC myTester;
+
+    @Before
+    public void setup(){
+        myTester = new WC(WC.class.getResource("/someTextFile.txt").getFile());
+    }
+
+
+
+    @Test
+    public void testWC(){
+        myTester.splitter();
+    }
+
+
 
 }
