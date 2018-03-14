354c6974d1f6f68988e3833b484e680af9dc382b
Kthomas <kthomas@github.com>
2018-03-13 20:32:54 -0400

    Part 1 and 2 Done

--- src/main/java/io/zipcoder/ParenChecker.java
+++ src/main/java/io/zipcoder/ParenChecker.java
@@ -1,4 +1,40 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class ParenChecker {
+    public ParenChecker(){
+    }
+
+    public boolean braceChecker(String str) {
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0; i < str.length(); i++) {
+            char openCharacter = str.charAt(i);
+            if (openCharacter == '[' || openCharacter == '(' || openCharacter == '{' || openCharacter == '<' || openCharacter == '"') {
+                stack.push(openCharacter);
+            } else if (openCharacter == ']') {
+                if (stack.isEmpty() || stack.pop() != '[') {
+                    return false;
+                }
+            } else if (openCharacter == ')') {
+                if (stack.isEmpty() || stack.pop() != '(') {
+                    return false;
+                }
+            } else if (openCharacter == '}') {
+                if (stack.isEmpty() || stack.pop() != '{') {
+                    return false;
+                }
+            } else if (openCharacter == '>') {
+                if (stack.isEmpty() || stack.pop() != '<') {
+                    return false;
+                }
+            } else if (openCharacter == '"') {
+                if (stack.isEmpty() || stack.pop() != '"') {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
 }
--- src/main/java/io/zipcoder/WC.java
+++ src/main/java/io/zipcoder/WC.java
@@ -2,8 +2,18 @@ package io.zipcoder;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.Iterator;
-import java.util.Scanner;
+import java.util.*;
+
+/**
+@TODO
+*@1.)Create a parenthesis verifier method, that has one job(check if all parenthesis are ('paired')
+* @2.)Create a method that checks all opening characters have a closing one
+* - These Characters () {} [] <> "" ''
+* This program should be able to:
+ * 1 - count all the words in a file
+ * 2 - Print out all the words
+ * 3 - Print the word counts in descending order
+ */
 
 public class WC {
     private Iterator<String> si;
@@ -21,3 +31,30 @@ public class WC {
         this.si = si;
     }
 }
+
+//    private static boolean isPaired(String str) {
+//        /*stores braces*/
+//        Map<Character, Character> bracketPairs = new HashMap<Character, Character>(){{
+//                put('(', ')');
+//                put('{', '}');
+//                put('[', ']');
+//                put('<', '>');
+//                put('"', '"');
+//        }
+//    };
+//
+//        if(str.length() % 2 != 0) { //a pair will donate a balance of at least 2 characters/braces
+//            return false;
+//        }
+//
+//            Stack<Character> openBrace = new Stack<>(); //store the open brace
+//            for(int i = 0; i < str.length(); i++){
+//            if(bracketPairs.containsKey(str.charAt(i))){ //if the character is open,
+//                openBrace.push(str.charAt(i)); // store it into the stack
+//            }
+//            else if(openBrace.isEmpty() || bracketPairs.get(openBrace.pop()) != str.charAt(i)){ //if the closing character doesnt match the top of the stack or is empty, return false
+//                return false;
+//            }
+//        }
+//        return openBrace.isEmpty() ? true : false;
+//    }
