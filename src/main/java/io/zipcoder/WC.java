6d087c38d229873adeee4e7dadbb87d87f31ed05
Jessica Campbell <jessicacampbell@zipcoders-MacBook-Pro-42.local>
2018-03-14 11:52:02 -0400

    text added

--- src/main/java/io/zipcoder/WC.java
+++ src/main/java/io/zipcoder/WC.java
@@ -2,17 +2,22 @@ package io.zipcoder;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.Map;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.Scanner;
+import static java.util.stream.Collectors.toMap;
 
 public class WC {
+    
     private Iterator<String> si;
+    private Map <String, Integer> wordsWithCounts = new LinkedHashMap<>();
 
     public WC(String fileName) {
         try {
             this.si = new Scanner(new FileReader(fileName));
         } catch (FileNotFoundException e) {
-            System.out.println(fileName + " Does Not Exist");
+            System.out.println(fileName + "Does Not Exist");
             System.exit(-1);
         }
     }
@@ -20,4 +25,28 @@ public class WC {
     public WC(Iterator<String> si) {
         this.si = si;
     }
+
+    public void wordCount() {
+        while (si.hasNext()) {
+            String current = si.next().toLowerCase().replaceAll("[a-zñáéíóú]", ""); // deletes all other special characters and replaces them w blank space
+            if (wordsWithCounts.containsKey(current)) {
+                wordsWithCounts.put(current, wordsWithCounts.get(current) + 1); // if we have this word -> get the int value at that key and add one
+            } else {
+                wordsWithCounts.put(current, 1); // if we do not have the word, add it and set the count to one
+            }
+        }
+    }
+
+    public void print(){
+        this.wordsWithCounts.entrySet()
+                .stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                .forEach(System.out::println);
+    }
+
+    public static void main (String[] args){
+        WC wc = new WC("/Users/jessicacampbell/Dev/EncapsulativeCharacters/src/main/resources/DonQuijote.txt");
+        wc.print();
+    }
+
 }
--- src/test/java/io/zipcoder/ParenCheckerTest.java
+++ src/test/java/io/zipcoder/ParenCheckerTest.java
@@ -1,44 +1,44 @@
-package io.zipcoder;
-
-import org.junit.Assert;
-import org.junit.Test;
-
-public class ParenCheckerTest {
-
-
-    public ParenCheckerTest() {
-    }
-
-    @Test
-    public void testParenCheck() {
-        //given
-        ParenChecker parenChecker = new ParenChecker();
-        //when
-        String input = "ab(c)d(";
-        //then
-        boolean actual = parenChecker.parenCheck(input);
-        Assert.assertFalse(actual);
-    }
-
-    @Test
-    public void testIsPair() {
-        //given
-        ParenChecker parenChecker = new ParenChecker();
-        //when
-        String input = "(xoxo)";
-        //then
-        boolean actual = parenChecker.parenCheck(input);
-        Assert.assertTrue(actual);
-    }
-    @Test
-    public void testIsPair1() {
-        //given
-        ParenChecker parenChecker = new ParenChecker();
-        //when
-        String input = "<sdkls<e>sdokf>";
-        //then
-        boolean actual = parenChecker.parenCheck(input);
-        Assert.assertFalse(actual);
-    }
-}
+//package io.zipcoder;
+//
+//import org.junit.Assert;
+//import org.junit.Test;
+//
+//public class ParenCheckerTest {
+//
+//
+//    public ParenCheckerTest() {
+//    }
+//
+//    @Test
+//    public void testParenCheck() {
+//        //given
+//        ParenChecker parenChecker = new ParenChecker();
+//        //when
+//        String input = "ab(c)d(";
+//        //then
+//        boolean actual = parenChecker.parenCheck(input);
+//        Assert.assertTrue(actual);
+//    }
+//
+//    @Test
+//    public void testIsPair() {
+//        //given
+//        ParenChecker parenChecker = new ParenChecker();
+//        //when
+//        String input = "(xoxo)";
+//        //then
+//        boolean actual = parenChecker.isPair(input);
+//        Assert.assertTrue(actual);
+//    }
+//    @Test
+//    public void testIsPair1() {
+//        //given
+//        ParenChecker parenChecker = new ParenChecker();
+//        //when
+//        String input = "<sdkls<e>sdokf";
+//        //then
+//        boolean actual = parenChecker.isPair(input);
+//        Assert.assertFalse(actual);
+//    }
+//}
 
